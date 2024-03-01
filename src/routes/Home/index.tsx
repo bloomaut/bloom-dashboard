0da709c2afa0f59bcee276c5ddbfee39f9cdc65a
Franco Galluccio <francoivangalluccio@gmail.com>
2024-03-01 17:55:34 -0300

    ADD Logout

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,7 +1,12 @@
 import styles from "./styles.module.scss";
 
 const HomePage = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <h1>Home Page</h1>
+      <a href='/api/auth/logout'>Logout</a>
+    </>
+  );
 };
 
 export default HomePage;
--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,7 +1,11 @@
 import styles from "./styles.module.scss";
 
 const Login = () => {
-  return <a href='/api/auth/login'>Login</a>;
+  return (
+    <section className={styles.container}>
+      <a href='/api/auth/login'>Login</a>
+    </section>
+  );
 };
 
 export default Login;
