6fb19b0b8856fa131c5641276180667e0e6dd15a
rumrage <claravillarruel4@gmail.com>
2024-03-06 17:11:25 -0300

    ADD knowledge button

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./styles.module.scss";
 import Image from "next/image";
 import small from "@/../public/assets/Small.png";
+import logo_knowledge from "@/../public/icons/KnowledgeIcon.png";
 import { Link } from "@/navigation";
 
 const Navbar = () => {
@@ -9,6 +10,19 @@ const Navbar = () => {
       <Link href='/'>
         <Image src={small} alt='Small' className={styles.logo} height={60} width={166} priority />
       </Link>
+      <div className={styles.inner_container}>
+        {/* Knowledge */}
+        <Link href='https://noti-knowledge.vercel.app/es/' target='_blank'>
+          <div className={styles.knowledge}>
+            <Image src={logo_knowledge} alt='Logo' priority width={26} height={22} className={styles.logo_knowledge} />
+            <p className={styles.text_knowledge}>Knowledge</p>
+          </div>
+        </Link>
+
+        <Link href='/'>Lang Drop</Link>
+        <Link href='/'>Icon</Link>
+        <Link href='/'>User</Link>
+      </div>
     </nav>
   );
 };
