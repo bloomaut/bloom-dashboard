672c08f85d0e623a73d1ea2b06de0d4263451d50
diaslucia <luciadias.dev@gmail.com>
2024-03-08 12:06:16 -0300

    ADD new component

--- src/routes/Home/Banner/index.tsx
+++ src/routes/Home/Banner/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import styles from "./styles.module.scss";
 import banner_home from "/public/assets/home_banner.png";
 import { useTranslations } from "next-intl";
-import Button from "@/components/Button";
+import LinkComponent from "@/components/LinkComponent";
 
 const Banner = () => {
   const dict = useTranslations("dict.home.banner");
@@ -13,9 +13,9 @@ const Banner = () => {
       <h3 className={styles.welcome}>{dict("welcome")}</h3>
       <article className={styles.banner}>
         <div className={styles.banner_text}>
-          <h1 className={styles.title}>{dict("title")}</h1>
+          <h4 className={styles.title}>{dict("title")}</h4>
           <p className={styles.paragraph}>{dict("paragraph")}</p>
-          <Button href='/' title={dict("btn")} />
+          <LinkComponent href='/' title={dict("btn")} />
         </div>
         <div className={styles.banner_img}>
           <Image src={banner_home} alt='Banner' />
