c89171ae1c99a649b6b5f40f3991ad27ef86e06f
Franco Galluccio <fgalluccio@jst.gob.ar>
2024-03-11 10:57:31 -0300

    ADD PopupShare

---
+++ src/components/PopupShare/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./styles.module.scss";
+import { useCloseDropdown } from "@/hooks/useCloseDropdown";
+
+interface PopupShareProps {
+  setShowPopup: (value: boolean) => void;
+}
+
+const PopupShare = ({ setShowPopup }: PopupShareProps) => {
+  const { dropdownRef } = useCloseDropdown(setShowPopup);
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.inner_container} ref={dropdownRef}>
+        <h2>Soy un Popup</h2>
+      </div>
+    </section>
+  );
+};
+
+export default PopupShare;
