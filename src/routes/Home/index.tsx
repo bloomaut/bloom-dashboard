b2c4769419cd1021593f5161de83300efc752cea
Franco Galluccio <fgalluccio@jst.gob.ar>
2024-03-12 10:42:59 -0300

    ADD hook useEnv

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,7 +1,27 @@
+import { get } from "@/services/fetch";
 import Banner from "./Banner";
 import styles from "./styles.module.scss";
+import { useEffect } from "react";
+import useEnv from "@/hooks/useEnv";
 
 const HomePage = () => {
+  useEnv(".env.secondary");
+
+  useEffect(() => {
+    const handleFetch = async () => {
+      try {
+        const response = await get("user/me");
+        if (response?.statusCode === 200) {
+          console.log(response);
+        }
+      } catch (error) {
+        console.log("Hacer algo con error", error);
+      }
+    };
+
+    handleFetch();
+  }, []);
+
   return (
     <section className={styles.container}>
       <Banner />
