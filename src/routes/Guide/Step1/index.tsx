2b33bf03a24cea05207746681d98240e61a43425
rumrage <claravillarruel4@gmail.com>
2024-03-12 17:39:41 -0300

    ADD logic steps

--- src/routes/Guide/Step1/index.tsx
+++ src/routes/Guide/Step1/index.tsx
@@ -2,17 +2,51 @@
 import styles from "./styles.module.scss";
 import { useTranslations } from "next-intl";
 import Steps from "../Steps";
+import { useState } from "react";
 
 const Step1 = () => {
   const dict = useTranslations("dict.guide.stepper_one");
+  const [activeStep, setActiveStep] = useState(1);
+  const handleStepClick = (stepNumber: number) => {
+    setActiveStep(stepNumber);
+  };
+
   return (
     <div className={styles.container}>
       <h1>Pasos</h1>
       <div className={styles.step_container}>
-        <Steps step_number={1} title={dict("title_one")} subtitle={dict("subtitle_one")} />
-        <Steps step_number={2} title={dict("title_two")} subtitle={dict("subtitle_two")} />
-        <Steps step_number={3} title={dict("title_three")} subtitle={dict("subtitle_three")} />
-        <Steps step_number={4} title={dict("title_four")} subtitle={dict("subtitle_four")} />
+        <div onClick={() => handleStepClick(1)}>
+          <Steps
+            step_number={1}
+            title={dict("title_one")}
+            subtitle={dict("subtitle_one")}
+            isActive={activeStep === 1}
+          />
+        </div>
+        <div onClick={() => handleStepClick(2)}>
+          <Steps
+            step_number={2}
+            title={dict("title_two")}
+            subtitle={dict("subtitle_two")}
+            isActive={activeStep === 2}
+          />
+        </div>
+        <div onClick={() => handleStepClick(3)}>
+          <Steps
+            step_number={3}
+            title={dict("title_three")}
+            subtitle={dict("subtitle_three")}
+            isActive={activeStep === 3}
+          />
+        </div>
+        <div onClick={() => handleStepClick(4)}>
+          <Steps
+            step_number={4}
+            title={dict("title_four")}
+            subtitle={dict("subtitle_four")}
+            isActive={activeStep === 4}
+          />
+        </div>
       </div>
     </div>
   );
--- src/routes/Guide/Steps/index.tsx
+++ src/routes/Guide/Steps/index.tsx
@@ -1,23 +1,30 @@
 import Image from "next/image";
 import styles from "./styles.module.scss";
 import green_check from "@/../public/icons/green_check.png";
+import gray_check from "@/../public/icons/gray_check.png";
 
 interface StepsProps {
   step_number: number;
   title: string;
   subtitle: string;
+  isActive: boolean;
 }
 
-const Steps = ({ step_number, title, subtitle }: StepsProps) => {
+const Steps = ({ step_number, title, subtitle, isActive }: StepsProps) => {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${isActive ? styles.active : styles.inactive}`}>
       <h1 className={styles.step_number}>{step_number}</h1>
       <div className={styles.text_container}>
         <h2>{title} </h2>
         <p>{subtitle} </p>
       </div>
       <div className={styles.check_container}>
-        <Image src={green_check} alt='Green check' width={39} height={30} />
+        <Image
+          src={isActive ? green_check : gray_check}
+          alt={isActive ? "Green check" : "Gray check"}
+          width={39}
+          height={30}
+        />
       </div>
     </div>
   );
