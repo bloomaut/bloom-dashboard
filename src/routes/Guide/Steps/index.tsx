a923826ca31648d6e5f9395acbf4d85ce33d677e
rumrage <claravillarruel4@gmail.com>
2024-03-12 16:07:59 -0300

    ADD final version

--- src/routes/Guide/Steps/index.tsx
+++ src/routes/Guide/Steps/index.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import styles from "./styles.module.scss";
+import green_check from "@/../public/icons/green_check.png";
 
 interface StepsProps {
   step_number: number;
@@ -13,6 +15,9 @@ const Steps = ({ step_number, title, subtitle }: StepsProps) => {
       <div className={styles.text_container}>
         <h2>{title} </h2>
         <p>{subtitle} </p>
+      </div>
+      <div className={styles.check_container}>
+        <Image src={green_check} alt='Green check' width={39} height={30} />
       </div>
     </div>
   );
