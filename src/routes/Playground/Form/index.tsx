94960290fdbce4c714bcf77e3420ac298542a40a
Gabriel <gszcode@gmail.com>
2024-03-12 15:02:41 -0300

    ADD playground layout

--- src/routes/Playground/Form/index.tsx
+++ src/routes/Playground/Form/index.tsx
@@ -3,16 +3,15 @@ import styles from "./styles.module.scss";
 
 interface PropsForm {
   setUrl: (url: string) => void;
-  url: string
+  url: string;
   submitForm: (e: React.FormEvent<HTMLFormElement>) => void;
 }
 
-const Form = ({setUrl, url, submitForm} : PropsForm) => {
-
+const Form = ({ setUrl, url, submitForm }: PropsForm) => {
   return (
     <div className={styles.container}>
       <form onSubmit={submitForm}>
-        <input type='text' value={url} onChange={(e) => setUrl(e.target.value)} placeholder='Enter URL' />
+        <input type='text' value={url} onChange={e => setUrl(e.target.value)} placeholder='Enter URL' />
         <button type='submit'>Generate Preview</button>
       </form>
     </div>
--- src/routes/Playground/TemplatesSelector/index.tsx
+++ src/routes/Playground/TemplatesSelector/index.tsx
@@ -1,11 +1,15 @@
-"use client";
 import styles from "./styles.module.scss";
 
 const TemplatesSelector = () => {
-
   return (
     <div className={styles.container}>
-     
+      <div className={styles.template_container}>
+        <h4 className={styles.title}>Título del Skin</h4>
+        <div className={styles.template}>
+          <div className={styles.sm_card}></div>
+          <div className={styles.lg_card}></div>
+        </div>
+      </div>
     </div>
   );
 };
