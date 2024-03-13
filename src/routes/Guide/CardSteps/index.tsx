1603721af057bae53cca360397605a985bb14272
diaslucia <luciadias.dev@gmail.com>
2024-03-13 14:54:10 -0300

    STYLE divs to fit properly on different screens

--- src/routes/Guide/CardSteps/index.tsx
+++ src/routes/Guide/CardSteps/index.tsx
@@ -14,23 +14,21 @@ interface StepsProps {
 
 const CardSteps = ({ step_number, title, subtitle, isActive, linkTo }: StepsProps) => {
   return (
-    <Link href={linkTo}>
-      <div className={`${styles.main_container} ${isActive ? styles.active : styles.inactive}`}>
+    <Link href={linkTo} className={`${styles.main_container} ${isActive ? styles.active : styles.inactive}`}>
+      <div className={styles.container}>
         <span className={styles.step_number}>{step_number}</span>
-        <div className={styles.container}>
-          <div className={styles.text_container}>
-            <h1>{title} </h1>
-            <p>{subtitle} </p>
-          </div>
-          <div className={styles.check_container}>
-            <Image
-              src={isActive ? green_check : gray_check}
-              alt={isActive ? "Green check" : "Gray check"}
-              width={39}
-              height={30}
-            />
-          </div>
+        <div className={styles.text_container}>
+          <h1>{title} </h1>
+          <p>{subtitle} </p>
         </div>
+      </div>
+      <div className={styles.check_container}>
+        <Image
+          src={isActive ? green_check : gray_check}
+          alt={isActive ? "Green check" : "Gray check"}
+          width={39}
+          height={30}
+        />
       </div>
     </Link>
   );
