5cb65a75a0224e9908ff53ba0ebfe1fc1be75461
rumrage <claravillarruel4@gmail.com>
2024-03-13 15:50:17 -0300

    ADD seconds sections steps and ADD button

--- src/routes/Guide/Step2/index.tsx
+++ src/routes/Guide/Step2/index.tsx
@@ -1,11 +1,58 @@
 import styles from "./styles.module.scss";
+import { useTranslations, useLocale } from "next-intl";
+import { useState } from "react";
+import CardSteps from "../CardSteps";
+import Button from "@/components/Button";
 
 interface Steps2Props {
   userStep: number;
 }
 
 const Step2 = ({ userStep }: Steps2Props) => {
-  return <div className={styles.container}>paso 2</div>;
+  const dict = useTranslations("dict.guide.stepper_two");
+  const locale = useLocale();
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+  const handleButtonClick = () => {
+    setIsButtonDisabled(false);
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>{dict("title")}</h1>
+      <div className={styles.step_container}>
+        <CardSteps
+          step_number={1}
+          title={dict("title_one")}
+          subtitle={dict("subtitle_one")}
+          isActive={userStep >= 5}
+          linkTo={`/${locale}/`}
+        />
+        <CardSteps
+          step_number={2}
+          title={dict("title_two")}
+          subtitle={dict("subtitle_two")}
+          isActive={userStep >= 6}
+          linkTo={`/${locale}/`}
+        />
+        <CardSteps
+          step_number={3}
+          title={dict("title_three")}
+          subtitle={dict("subtitle_three")}
+          isActive={userStep >= 7}
+          linkTo={`/${locale}/`}
+        />
+        <div className={styles.btn_container}>
+          <Button
+            title={dict("button")}
+            onclick={handleButtonClick}
+            isDisabled={isButtonDisabled}
+            styleName={isButtonDisabled ? "btn_disabled_sequence" : "btn"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Step2;
