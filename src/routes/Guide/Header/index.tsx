2dd166ddcbf4ae08aa91fa3cfe0eb423158d8d6d
rumrage <claravillarruel4@gmail.com>
2024-03-13 13:26:18 -0300

    ADD user step

--- src/routes/Guide/Header/index.tsx
+++ src/routes/Guide/Header/index.tsx
@@ -1,4 +1,3 @@
-"use client";
 import styles from "./styles.module.scss";
 import { useTranslations } from "next-intl";
 
--- src/routes/Guide/Step1/index.tsx
+++ src/routes/Guide/Step1/index.tsx
@@ -1,58 +1,62 @@
-"use client";
 import styles from "./styles.module.scss";
-import { useTranslations } from "next-intl";
+import { useTranslations, useLocale } from "next-intl";
 import Steps from "../Steps";
-import { useState } from "react";
-import { useLocale } from "next-intl";
+import { useState, useEffect } from "react";
+import { get } from "@/services/fetch";
 
 const Step1 = () => {
   const dict = useTranslations("dict.guide.stepper_one");
   const locale = useLocale();
-  const [activeStep, setActiveStep] = useState(1);
-  const handleStepClick = (stepNumber: number) => {
-    setActiveStep(stepNumber);
-  };
+
+  const [activeStep, setActiveStep] = useState(null);
+
+  useEffect(() => {
+    const fetchUserStep = async () => {
+      try {
+        const userData = await get("small-business/me", "NEXT_PUBLIC_API_DASH");
+        //console.log("User Data ok:", userData);
+        const userStep = userData.result.data.smallBusiness.step;
+        console.log("User step:", userStep);
+        setActiveStep(userStep);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+    fetchUserStep();
+  }, []);
 
   return (
     <div className={styles.container}>
       <h1>{dict("title")}</h1>
       <div className={styles.step_container}>
-        <div onClick={() => handleStepClick(1)}>
-          <Steps
-            step_number={1}
-            title={dict("title_one")}
-            subtitle={dict("subtitle_one")}
-            isActive={activeStep === 1}
-            linkTo={`/${locale}/introduction`}
-          />
-        </div>
-        <div onClick={() => handleStepClick(2)}>
-          <Steps
-            step_number={2}
-            title={dict("title_two")}
-            subtitle={dict("subtitle_two")}
-            isActive={activeStep === 2}
-            linkTo={`/${locale}/playground`}
-          />
-        </div>
-        <div onClick={() => handleStepClick(3)}>
-          <Steps
-            step_number={3}
-            title={dict("title_three")}
-            subtitle={dict("subtitle_three")}
-            isActive={activeStep === 3}
-            linkTo={`/${locale}/my-business`}
-          />
-        </div>
-        <div onClick={() => handleStepClick(4)}>
-          <Steps
-            step_number={4}
-            title={dict("title_four")}
-            subtitle={dict("subtitle_four")}
-            isActive={activeStep === 4}
-            linkTo={`/${locale}/design`}
-          />
-        </div>
+        <Steps
+          step_number={1}
+          title={dict("title_one")}
+          subtitle={dict("subtitle_one")}
+          isActive={activeStep === 1}
+          linkTo={`/${locale}/introduction`}
+        />
+        <Steps
+          step_number={2}
+          title={dict("title_two")}
+          subtitle={dict("subtitle_two")}
+          isActive={activeStep === 2}
+          linkTo={`/${locale}/playground`}
+        />
+        <Steps
+          step_number={3}
+          title={dict("title_three")}
+          subtitle={dict("subtitle_three")}
+          isActive={activeStep === 3}
+          linkTo={`/${locale}/my-business`}
+        />
+        <Steps
+          step_number={4}
+          title={dict("title_four")}
+          subtitle={dict("subtitle_four")}
+          isActive={activeStep === 4}
+          linkTo={`/${locale}/design`}
+        />
       </div>
     </div>
   );
