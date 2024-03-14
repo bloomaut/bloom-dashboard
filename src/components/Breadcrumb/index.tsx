7e66e01084989b7ef8b7565716fb63549c6201a5
diaslucia <luciadias.dev@gmail.com>
2024-03-14 16:40:05 -0300

    ADD dict in breadcrumb

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -2,6 +2,7 @@ import styles from "./styles.module.scss";
 import Image from "next/image";
 import ArrowIcon from "/public/icons/arrow_left.svg";
 import { useRouter } from "next/navigation";
+import { useTranslations } from "next-intl";
 
 interface BreadcrumbProps {
   title: string;
@@ -9,6 +10,7 @@ interface BreadcrumbProps {
 
 const Breadcrumb = ({ title }: BreadcrumbProps) => {
   const router = useRouter();
+  const dict = useTranslations("dict.breadcrumb");
 
   return (
     <ul className={styles.container}>
@@ -16,7 +18,7 @@ const Breadcrumb = ({ title }: BreadcrumbProps) => {
       <li>
         <button className={styles.btn} onClick={() => router.back()}>
           <Image src={ArrowIcon} alt='arrow' className={styles.arrow} width={15} height={20} />
-          <p className={styles.text}>Volver</p>
+          <p className={styles.text}>{dict("breadcrumb_link")}</p>
         </button>
       </li>
     </ul>
