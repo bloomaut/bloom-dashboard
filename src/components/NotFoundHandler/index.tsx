c1eeea801d7315cc5f54530c64efa94490077179
diaslucia <luciadias.dev@gmail.com>
2024-03-14 18:59:05 -0300

    ADJUST styles

--- src/components/NotFoundHandler/index.tsx
+++ src/components/NotFoundHandler/index.tsx
@@ -2,11 +2,19 @@ import styles from "./styles.module.scss";
 import Link from "next/link";
 import smallLogo from "@/../public/assets/Small.png";
 import Image from "next/image";
+import { Barlow } from "next/font/google";
+import "@/styles/globals.scss";
+
+const barlow = Barlow({
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const NotFoundHandler = () => {
   return (
     <html>
-      <body>
+      <body className={barlow.className}>
         <div className={styles.container}>
           <Image src={smallLogo} alt='' />
           <h1>Oops! Page not found</h1>
