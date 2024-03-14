d48fb33aeeabefb423f3179aca669859ee4123ef
rumrage <claravillarruel4@gmail.com>
2024-03-14 15:25:18 -0300

    FIX form values

--- src/routes/Business/Form/index.tsx
+++ src/routes/Business/Form/index.tsx
@@ -1,40 +1,58 @@
 "use client";
 import styles from "./styles.module.scss";
+import { useState } from "react";
 //Componentes
 import SectionTitle from "@/components/SectionTitle";
 import Input from "@/components/Input";
 
-const Form = () => {
+interface PropsForm {
+  submitForm: (e: React.FormEvent<HTMLFormElement>) => void;
+}
+
+const Form = ({ submitForm }: PropsForm) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    website: "",
+    description: "",
+    instagram: "",
+    phone: "",
+  });
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(handleChange);
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
+
   return (
     <div className={styles.container}>
       <SectionTitle text='Información del negocio' />
       <h4>*Mientras más campos se completen mejor será el resultado</h4>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={submitForm}>
         <Input
           textLabel='Nombre'
           textHolder='Ingrese el nombre'
           type='text'
-          name='nombre'
-          value=''
+          name='name'
+          value={formData.name}
           handleChange={handleChange}
         />
         <Input
           textLabel='Sitio web'
           textHolder='Ingrese el sitio web'
           type='text'
-          name='nombre'
-          value=''
+          name='website'
+          value={formData.website}
           handleChange={handleChange}
         />
         <Input
           textLabel='Descripción'
           textHolder='Ingrese una descripción'
           type='text'
-          name='nombre'
-          value=''
+          name='description'
+          value={formData.description}
           handleChange={handleChange}
           isDescription={true}
         />
@@ -42,16 +60,16 @@ const Form = () => {
           textLabel='Instagram'
           textHolder='Ingrese el instagram'
           type='text'
-          name='nombre'
-          value=''
+          name='instagram'
+          value={formData.instagram}
           handleChange={handleChange}
         />
         <Input
           textLabel='Teléfono'
           textHolder='Ingrese el teléfono'
           type='text'
-          name='nombre'
-          value=''
+          name='phone'
+          value={formData.phone}
           handleChange={handleChange}
         />
         <div className={styles.btn_container}>
--- src/routes/Business/index.tsx
+++ src/routes/Business/index.tsx
@@ -6,13 +6,16 @@ import Sequence from "./Sequence";
 import styles from "./styles.module.scss";
 
 const Business = () => {
+  const submitForm = (formData: any) => {
+    console.log(formData);
+  };
   return (
     <section className={styles.container}>
       <div className={styles.breadcrumb_container}>
         <Breadcrumb title={"Carga de Datos"} route={"playground"} />
       </div>
       <div className={styles.inner_container}>
-        <Form />
+        <Form submitForm={submitForm} />
         <Files />
         <Sequence />
       </div>
