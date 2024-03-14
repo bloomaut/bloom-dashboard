81ccfe32ba7945264413c8f99f3418d32de22142
rumrage <claravillarruel4@gmail.com>
2024-03-14 17:26:13 -0300

    ADD new styles

--- src/components/NotFoundHandler/index.tsx
+++ src/components/NotFoundHandler/index.tsx
@@ -1,11 +1,19 @@
+import styles from "./styles.module.scss";
 import Link from "next/link";
+import smallLogo from "@/../public/assets/Small.png";
+import Image from "next/image";
 
 const NotFoundHandler = () => {
   return (
     <html>
       <body>
-        <h1>Oops! Page not found</h1>
-        <Link href={"/"}>Go Back</Link>
+        <div className={styles.container}>
+          <Image src={smallLogo} alt='' />
+          <h1>Oops! Page not found</h1>
+          <Link href={"/"} className={styles.btn}>
+            Go Back
+          </Link>
+        </div>
       </body>
     </html>
   );
