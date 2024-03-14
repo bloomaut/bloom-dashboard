f27e01aa03dcd5f55bbb6ae40a5ad72fc6700d59
rumrage <claravillarruel4@gmail.com>
2024-03-14 11:02:54 -0300

    FIX steps logic

--- src/routes/Guide/index.tsx
+++ src/routes/Guide/index.tsx
@@ -12,19 +12,28 @@ const GuidePage = () => {
   const [activeStep, setActiveStep] = useState(1);
   const [step, setStep] = useState<number>(0);
   const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     const fetchUserStep = async () => {
       setIsLoading(true);
-      const userData = await get("small-business/me", "NEXT_PUBLIC_API_DASH");
-      if (userData.statusCode === 200) {
-        const userStep = userData.result.data.smallBusiness.step;
-        setStep(userStep);
-        setIsLoading(false);
-      } else {
-        console.log("Acá va a ir un toast");
+      try {
+        const userData = await get("small-business/me", "NEXT_PUBLIC_API_DASH");
+        if (userData.statusCode === 200) {
+          const userStep = userData.result.data.smallBusiness.step;
+          setStep(userStep);
+          setIsLoggedIn(true);
+        } else {
+          console.log("Acá va a ir un toast");
+          setIsLoggedIn(false);
+        }
+      } catch (error) {
+        console.error("Error al obtener datos del usuario:", error);
+        setIsLoggedIn(false);
       }
+      setIsLoading(false);
     };
+
     fetchUserStep();
   }, []);
 
@@ -33,10 +42,16 @@ const GuidePage = () => {
       <Header handleStepChange={setActiveStep} activeStep={activeStep} />
       {isLoading ? (
         <LoadingSpinner />
-      ) : (
+      ) : isLoggedIn ? ( // Si el usuario está logueado
         <>
           {activeStep === 1 && <Step1 userStep={step} />}
           {activeStep === 2 && <Step2 userStep={step} />}
+        </>
+      ) : (
+        // Si no está logueado, cargar pasos deshabilitados
+        <>
+          {activeStep === 1 && <Step1 userStep={0} />}
+          {activeStep === 2 && <Step2 userStep={0} />}
         </>
       )}
     </section>
