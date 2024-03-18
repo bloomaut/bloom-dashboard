e449c82f0a5d70ecd50dc48587b5d915d34697a0
Franco Galluccio <fgalluccio@jst.gob.ar>
2024-03-18 12:01:36 -0300

    ADD translations

--- src/routes/Business/Files/index.tsx
+++ src/routes/Business/Files/index.tsx
@@ -11,12 +11,14 @@ import { useMessageToast } from "@/hooks/useMessageToast";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { ENV } from "@/typescript/types/environment.enum";
 import { updateLogo } from "@/store/features/businessSlice";
+import { useTranslations } from "next-intl";
 
 const Files = () => {
   const companyLogo = useAppSelector(data => data.business.logo);
   const reduxFiles = useAppSelector(data => data.files.media);
   const [file, setFile] = useState<File | null>(null);
   const { notify, notifyError } = useMessageToast();
+  const dict = useTranslations("dict");
   const dispatch = useAppDispatch();
 
   const handleUpdateLogo = async (logoUrl: string) => {
@@ -25,10 +27,10 @@ const Files = () => {
         logo: logoUrl,
       };
       await update("small-business", dataToSend, ENV.DASH);
-      notify("Imagen subida correctamente");
+      notify(`${dict("toast.success_img")}`);
       dispatch(updateLogo(logoUrl));
     } catch (error) {
-      notifyError("Hubo un error al cargar la imagen");
+      notifyError(`${dict("toast.error_img")}`);
     }
   };
 
@@ -40,10 +42,10 @@ const Files = () => {
         const logoUrl = response.data.result.media.url;
         await handleUpdateLogo(logoUrl);
       } else {
-        notify("Archivo subido correctamente");
+        notify(`${dict("toast.success_file")}`);
       }
     } else {
-      notifyError("Hubo un error al cargar la imagen");
+      notifyError(`${dict("toast.error_img")}`);
     }
   };
 
@@ -65,7 +67,7 @@ const Files = () => {
 
   return (
     <div className={styles.container}>
-      <Subtitle text={!companyLogo ? "Subí tu logo para iniciar" : "Arrojá tus archivos aquí"} />
+      <Subtitle text={!companyLogo ? `${dict("business.file.title01")}` : `${dict("business.file.title02")}`} />
       <FileDragDrop setFile={setFile} />
 
       {/* Muestra siempre el logo*/}
@@ -81,7 +83,7 @@ const Files = () => {
         </div>
       )}
 
-      <Subtitle text={"Mis archivos"} />
+      <Subtitle text={`${dict("business.file.subtitle")}`} />
       <div className={styles.files}>
         {/* Muestra todos los archivos PDF */}
         {reduxFiles
--- src/routes/Business/Form/index.tsx
+++ src/routes/Business/Form/index.tsx
@@ -12,12 +12,20 @@ import Subtitle from "../Subtitle";
 import { update } from "@/services/fetch";
 import { setBusinessData } from "@/store/features/businessSlice";
 
+interface FormDataProps {
+  name: string;
+  website: string;
+  description: string;
+  instagram: string;
+  phone: string;
+}
+
 const Form = () => {
-  const dict = useTranslations("dict.business.form");
+  const dict = useTranslations("dict");
   const business = useAppSelector(data => data.business);
   const dispatch = useAppDispatch();
   const { notify, notifyError } = useMessageToast();
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<FormDataProps>({
     name: "",
     website: "",
     description: "",
@@ -48,10 +56,10 @@ const Form = () => {
       const response = await update("small-business", formData, ENV.DASH);
       if (response.data.statusCode === 200) {
         dispatch(setBusinessData(response.data.result.data));
-        notify("Información actualizada correctamente");
+        notify(`${dict("dict.success_edit")}`);
       }
     } catch (error) {
-      notifyError("Hubo un error al actualizar la información");
+      notifyError(`${dict("dict.error_edit")}`);
     }
   };
 
@@ -62,28 +70,28 @@ const Form = () => {
 
   return (
     <div className={styles.container}>
-      <Subtitle text={dict("title")} />
-      <h4>*{dict("subtitle")}</h4>
+      <Subtitle text={dict("business.form.title")} />
+      <h4>*{dict("business.form.subtitle")}</h4>
       <form className={styles.form} onSubmit={submitForm}>
         <Input
-          textLabel={dict("name")}
-          textHolder={dict("placeholder_name")}
+          textLabel={dict("business.form.name")}
+          textHolder={dict("business.form.placeholder_name")}
           type='text'
           name='name'
           value={formData.name}
           handleChange={handleChange}
         />
         <Input
-          textLabel={dict("website")}
-          textHolder={dict("placeholder_website")}
+          textLabel={dict("business.form.website")}
+          textHolder={dict("business.form.placeholder_website")}
           type='text'
           name='website'
           value={formData.website}
           handleChange={handleChange}
         />
         <Input
-          textLabel={dict("description")}
-          textHolder={dict("placeholder_description")}
+          textLabel={dict("business.form.description")}
+          textHolder={dict("business.form.placeholder_description")}
           type='text'
           name='description'
           value={formData.description}
@@ -92,15 +100,15 @@ const Form = () => {
         />
         <Input
           textLabel='Instagram'
-          textHolder={dict("placeholder_instagram")}
+          textHolder={dict("business.form.placeholder_instagram")}
           type='text'
           name='instagram'
           value={formData.instagram}
           handleChange={handleChange}
         />
         <Input
-          textLabel={dict("phone")}
-          textHolder={dict("placeholder_phone")}
+          textLabel={dict("business.form.phone")}
+          textHolder={dict("business.form.placeholder_phone")}
           type='text'
           name='phone'
           value={formData.phone}
