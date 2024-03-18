327d6f36125c6accc4269487267f6f3217b32d8d
rumrage <claravillarruel4@gmail.com>
2024-03-18 17:34:09 -0300

    ADD popupLogin to design

--- src/app/[locale]/(small)/design/page.tsx
+++ src/app/[locale]/(small)/design/page.tsx
@@ -1,8 +1,15 @@
 "use client";
-import { withPageAuthRequired } from "@auth0/nextjs-auth0/client";
+import { useState } from "react";
+import PopupLogin from "@/components/PopupLogin";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import LoadingSpinner from "@/components/Loading";
 
 function Page() {
-  return <h1>Design Page</h1>;
+  const [showPopup, setShowPopup] = useState(false);
+  const { user, isLoading } = useUser();
+  return (
+    <>{!isLoading ? user ? <h1>Design Page</h1> : <PopupLogin setShowPopup={setShowPopup} /> : <LoadingSpinner />}</>
+  );
 }
 
-export default withPageAuthRequired(Page);
+export default Page;
