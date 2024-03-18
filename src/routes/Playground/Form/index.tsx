dd8dd6ee693ac9f0ebc0c6c2f651cccab5be1aae
diaslucia <luciadias.dev@gmail.com>
2024-03-18 15:35:05 -0300

    ADD post form function

--- src/routes/Playground/Form/index.tsx
+++ src/routes/Playground/Form/index.tsx
@@ -84,6 +84,21 @@ const Form = () => {
     setFormDataPost(EmptyFormData);
   };
 
+  const postForm = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Lo que envio", formDataPost);
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_DASH}/hotlinks/playground`, {
+        method: "POST",
+        body: JSON.stringify(formDataPost),
+      });
+      console.log(response);
+    } catch (error) {
+      console.error("Error en la solicitud:", error);
+      throw error;
+    }
+  };
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
     const updatedFormInfo = [...formInfo];
     updatedFormInfo[index].value = e.target.value;
@@ -96,7 +111,7 @@ const Form = () => {
       {loading ? (
         <Loading />
       ) : (
-        <form className={styles.form} onSubmit={handleSubmit}>
+        <form className={styles.form} onSubmit={postForm}>
           {formInfo &&
             formInfo.map((info, index) => (
               <Input
