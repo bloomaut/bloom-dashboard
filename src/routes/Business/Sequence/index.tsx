df5ca9c79823d2a3b6112e4a6745a6c5b41d828e
Franco Galluccio <fgalluccio@jst.gob.ar>
2024-03-18 15:34:05 -0300

    FIX logic

--- src/routes/Business/Sequence/index.tsx
+++ src/routes/Business/Sequence/index.tsx
@@ -18,53 +18,56 @@ const Sequence = () => {
   const business = useAppSelector(data => data.business);
   const files = useAppSelector(data => data.files.media);
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
-  const [step1, setStep1] = useState(false);
-  const [step2, setStep2] = useState(true);
-  const [step3, setStep3] = useState(true);
-  const [step4, setStep4] = useState(true);
+  const [currentStep, setCurrentStep] = useState(1);
   const dict = useTranslations("dict.business.sequence");
 
   useEffect(() => {
-    if (business?.name) {
-      setStep2(false);
-    }
-    if (business?.name && business?.logo) {
-      setStep3(false);
-    }
-    if (business?.name && business?.logo && files?.length > 1) {
-      for (const file of files) {
-        if (file.filetype === "application/pdf") {
-          setStep4(false);
-          break;
+    switch (currentStep) {
+      case 1:
+        if (business?.name) {
+          setCurrentStep(2);
         }
-      }
+        break;
+      case 2:
+        if (business?.logo) {
+          setCurrentStep(3);
+        }
+        break;
+      case 3:
+        if (files?.length >= 2) {
+          const hasPDF = files.some(file => file.filetype === "application/pdf");
+          if (hasPDF) setCurrentStep(4);
+        }
+        break;
+      default:
+        break;
     }
-  }, [business, files]);
+  }, [business, files, currentStep]);
 
   const content: ContentProps[] = [
     {
       step: 1,
       title: `${dict("card_title_01")}`,
       description: `${dict("card_description_01")}`,
-      disabled: step1,
+      disabled: currentStep !== 1,
     },
     {
       step: 2,
       title: `${dict("card_title_02")}`,
       description: `${dict("card_description_02")}`,
-      disabled: step2,
+      disabled: currentStep !== 2,
     },
     {
       step: 3,
       title: `${dict("card_title_03")}`,
       description: `${dict("card_description_03")}`,
-      disabled: step3,
+      disabled: currentStep !== 3,
     },
     {
       step: 4,
       title: `${dict("card_title_04")}`,
       description: `${dict("card_description_04")}`,
-      disabled: step4,
+      disabled: currentStep !== 4,
     },
   ];
 
