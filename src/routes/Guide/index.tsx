e1220ec68d8c7371e82a89de6c543e6a2adb2fac
diaslucia <luciadias.dev@gmail.com>
2024-03-19 09:26:06 -0300

    FIX error in playground

--- src/routes/Guide/index.tsx
+++ src/routes/Guide/index.tsx
@@ -19,7 +19,6 @@ const GuidePage = () => {
     const fetchUserStep = async () => {
       setIsLoading(true);
       const userData = await get("small-business/me", ENV.DASH);
-      console.log(userData);
       if (userData.statusCode === 200) {
         const userStep = userData.result.data.smallBusiness.step;
         setStep(userStep);
