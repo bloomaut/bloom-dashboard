b956486d7335f77a6676b018f1d4ae0fdff88ae8
rumrage <claravillarruel4@gmail.com>
2024-03-25 12:24:01 -0300

    ADD link and qr code

--- src/routes/Inbox/index.tsx
+++ src/routes/Inbox/index.tsx
@@ -1,10 +1,33 @@
-import Title from "@/components/Title";
 import styles from "./styles.module.scss";
+import Image from "next/image";
+import { useState } from "react";
+import Title from "@/components/Title";
+import miniQr from "@/../public/assets/miniQr.png";
+import play from "@/../public/assets/play.png";
+import Link from "next/link";
+import QrModal from "./Qr";
 
 const InboxPage = () => {
+  const [showPopup, setShowPopup] = useState(false);
   return (
     <section className={styles.container}>
-      <Title text='Bandeja de Entrada' />
+      <div className={styles.title_container}>
+        <Title text='Bandeja de Entrada' />
+        <div className={styles.inner_container}>
+          <h5>Descarga la app</h5>
+          <div className={styles.box}>
+            <Link href='https://play.google.com/store/apps/details?id=com.notimation.agentcaller' target='_blank'>
+              <Image src={play} alt='Google Play Logo' width={25} height={25} />
+            </Link>
+            <button onClick={() => setShowPopup(!showPopup)}>
+              <Image src={miniQr} alt='QR' width={25} height={25} />
+            </button>
+
+            {showPopup && <QrModal setShowPopup={() => setShowPopup(!showPopup)} />}
+          </div>
+        </div>
+      </div>
+
       <iframe src='https://notimation.com/es/agents' title='Agents App' width='950' height='550'></iframe>
     </section>
   );
