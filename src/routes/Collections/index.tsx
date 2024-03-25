7de05191e03a1bce3a7f21e6af97fadfcc2ed762
rumrage <claravillarruel4@gmail.com>
2024-03-25 15:34:57 -0300

    ADD buttons and inputs

--- src/routes/Collections/index.tsx
+++ src/routes/Collections/index.tsx
@@ -1,7 +1,44 @@
+import Title from "@/components/Title";
 import styles from "./styles.module.scss";
+import down from "@/../public/icons/IconDown.svg";
+import bar from "@/../public/icons/bar.svg";
+import period from "@/../public/icons/period.svg";
+import plus from "@/../public/icons/plus.svg";
+import Image from "next/image";
+import { useLocale, useTranslations } from "next-intl";
+import Link from "next/link";
 
 const CollectionsPage = () => {
-  return <section>Collections Page</section>;
+  const locale = useLocale();
+  const dict = useTranslations("dict.collections");
+
+  return (
+    <section className={styles.container}>
+      <Title text={dict("title")} />
+      <div className={styles.inputs_container}>
+        <div className={styles.search_container}>
+          <input type='search' placeholder={dict("search")} className={styles.search} />
+          <div className={styles.search_icon} />
+        </div>
+        <div className={styles.period_container}>
+          <button className={styles.period}>
+            {dict("period_btn")}
+            <Image src={down} alt='' />
+          </button>
+
+          <div className={styles.period}>
+            <Image src={bar} alt='' />
+            <Image src={period} alt='' />
+          </div>
+        </div>
+        <Link href={`/${locale}/`} className={styles.new_btn}>
+          <Image src={plus} alt='' width={15} height={15} />
+          {dict("new_btn")}
+        </Link>
+      </div>
+      <div>tabla</div>
+    </section>
+  );
 };
 
 export default CollectionsPage;
