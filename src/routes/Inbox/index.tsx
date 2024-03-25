442d02f1db647732758846c246c3b4a466f8b933
rumrage <claravillarruel4@gmail.com>
2024-03-25 11:04:42 -0300

    ADD title and iframe

--- src/routes/Inbox/index.tsx
+++ src/routes/Inbox/index.tsx
@@ -1,7 +1,13 @@
+import Title from "@/components/Title";
 import styles from "./styles.module.scss";
 
 const InboxPage = () => {
-  return <section>Inbox Page</section>;
+  return (
+    <section className={styles.container}>
+      <Title text='Bandeja de Entrada' />
+      <iframe src='https://notimation.com/es/agents' title='Agents App' width='950' height='550'></iframe>
+    </section>
+  );
 };
 
 export default InboxPage;
