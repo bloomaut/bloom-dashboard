ea8739dc80de2a2f63c8642db379ec520f62d30b
Gaby Sanchez <gszcode@gmail.com>
2024-03-26 17:34:02 -0300

    ADD gallery page

---
+++ src/routes/Gallery/GalleryComponent/index.tsx
@@ -0,0 +1,43 @@
+import styles from "./styles.module.scss";
+import Image from "next/image";
+//Componentes
+import Loading from "@/app/[locale]/(playground)/introduction/loading";
+import { useGalleryContext } from "@/context/GalleryContext";
+import { Powerapp } from "@/typescript/interfaces/flakes.interface";
+import HogIcon from "@/routes/Playground/TemplatesSelector/Icons/Hog";
+
+const GalleryComponent = () => {
+  const { flakes, loading } = useGalleryContext();
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.flakes}>
+        {!loading ? (
+          flakes?.map((app: Powerapp) => (
+            <div className={styles.template_container} key={app._id}>
+              <h4 className={styles.title}>{app.skinx.title}</h4>
+              <div className={styles.template}>
+                <div className={styles.sm_card}>
+                  {app.hog_related.thumbnail ? (
+                    <Image src={app.hog_related.thumbnail} alt={app.skinx.title} width={167} height={120} />
+                  ) : (
+                    <HogIcon />
+                  )}
+                </div>
+                <div className={styles.lg_card}>
+                  {app.thumbnail && <Image src={app.thumbnail} alt={app.skinx.title} width={137} height={100} />}
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className={styles.loader}>
+            <Loading />
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default GalleryComponent;
---
+++ src/routes/Gallery/index.tsx
@@ -0,0 +1,32 @@
+import styles from "./styles.module.scss";
+import Title from "@/components/Title";
+import Image from "next/image";
+import Link from "next/link";
+import GalleryComponent from "./GalleryComponent";
+import { GalleryProvider } from "@/context/GalleryContext";
+import { useTranslations } from "next-intl";
+
+const GalleryPage = () => {
+  const dict = useTranslations("dict.gallery");
+
+  return (
+    <GalleryProvider>
+      <section className={styles.gallery}>
+        <div className={styles.head}>
+          <Title text={dict("title")} />
+          <div className={styles.logo}>
+            <p className={styles.logo_text}>{dict("more_designs")}</p>
+            <Link href=''>
+              <Image src='/assets/logo_uitrade.png' alt='logo' width={130} height={40} priority />
+            </Link>
+          </div>
+        </div>
+        <div className={styles.inner_container}>
+          <GalleryComponent />
+        </div>
+      </section>
+    </GalleryProvider>
+  );
+};
+
+export default GalleryPage;
