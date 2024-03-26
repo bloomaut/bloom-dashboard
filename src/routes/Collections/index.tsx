776549c147ec03e2f634da095b38c346322af58e
rumrage <claravillarruel4@gmail.com>
2024-03-26 13:47:23 -0300

    ADD title

--- src/routes/Collections/index.tsx
+++ src/routes/Collections/index.tsx
@@ -38,19 +38,12 @@ const CollectionsPage = () => {
       </div>
       <div className={styles.table}>
         <div className={styles.header}>
-          <h2>Nombre</h2>
-          <h2>Diseño Utilizado</h2>
-          <h2>Receptor</h2>
+          <div className={styles.title}>
+            <h2>Nombre</h2>
+            <h2 className={styles.border}>Diseño utilizado</h2>
+            <h2 className={styles.border}>Receptor</h2>
+          </div>
         </div>
-        {/*
-        <div className={styles.row}>
-          <h5>Fecha</h5>
-          <h5>Nombre del Skin</h5>
-          <h5>Plantilla</h5>
-          <h5>Total</h5>
-          <h5>Abiertos</h5>
-        </div>
-         */}
       </div>
     </section>
   );
