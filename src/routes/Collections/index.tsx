a93307f292164c679af1be97a0503ae50a0d0fe8
rumrage <claravillarruel4@gmail.com>
2024-03-26 14:23:25 -0300

    ADD Header components

--- src/routes/Collections/index.tsx
+++ src/routes/Collections/index.tsx
@@ -1,12 +1,6 @@
-import Title from "@/components/Title";
+import Header from "./Header";
 import styles from "./styles.module.scss";
-import down from "@/../public/icons/IconDown.svg";
-import bar from "@/../public/icons/bar.svg";
-import period from "@/../public/icons/period.svg";
-import plus from "@/../public/icons/plus.svg";
-import Image from "next/image";
 import { useLocale, useTranslations } from "next-intl";
-import Link from "next/link";
 
 const CollectionsPage = () => {
   const locale = useLocale();
@@ -14,28 +8,7 @@ const CollectionsPage = () => {
 
   return (
     <section className={styles.container}>
-      <Title text={dict("title")} />
-      <div className={styles.inputs_container}>
-        <div className={styles.search_container}>
-          <input type='search' placeholder={dict("search")} className={styles.search} />
-          <div className={styles.search_icon} />
-        </div>
-        <div className={styles.period_container}>
-          <button className={styles.period}>
-            {dict("period_btn")}
-            <Image src={down} alt='' />
-          </button>
-
-          <div className={styles.period}>
-            <Image src={bar} alt='' />
-            <Image src={period} alt='' />
-          </div>
-        </div>
-        <Link href={`/${locale}/`} className={styles.new_btn}>
-          <Image src={plus} alt='' width={15} height={15} />
-          {dict("new_btn")}
-        </Link>
-      </div>
+      <Header />
       <div className={styles.table}>
         <div className={styles.header}>
           <div className={styles.title}>
