ade2c4f91fae9ff2cb356c44477a2ef8c03d2208
rumrage <claravillarruel4@gmail.com>
2024-03-27 11:53:20 -0300

    ADD select and components

---
+++ src/routes/Hotlink/ListHotlinks/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListHotlinks = () => {
+  return <div>ListHotlinks</div>;
+};
+
+export default ListHotlinks;
--- src/routes/Hotlink/index.tsx
+++ src/routes/Hotlink/index.tsx
@@ -1,7 +1,19 @@
+import Select from "./Select";
 import styles from "./styles.module.scss";
+import Title from "@/components/Title";
+import { FlakesProvider } from "@/context/FlakesContext";
 
 const HotlinksPage = () => {
-  return <section>Hotlinks Page</section>;
+  return (
+    <FlakesProvider>
+      <section className={styles.container}>
+        <Title text='Generación Hotlink' />
+        <div className={styles.inner_container}>
+          <Select />
+        </div>
+      </section>
+    </FlakesProvider>
+  );
 };
 
 export default HotlinksPage;
