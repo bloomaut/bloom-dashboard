1112fd74a28134e86fbb323f1a447351c1d3f0de
Gaby Sanchez <gszcode@gmail.com>
2024-03-28 12:42:09 -0300

    ADD useFlakesUser custom hook

---
+++ src/hooks/useFlakesUser.ts
@@ -0,0 +1,30 @@
+import { get } from "@/services/fetch";
+import { Powerapp } from "@/typescript/interfaces/flakes.interface";
+import { useEffect, useState } from "react";
+import { useMessageToast } from "./useMessageToast";
+import { useTranslations } from "next-intl";
+import { ENV } from "@/typescript/types/environment.enum";
+
+export const useFlakeData = () => {
+  const [flakes, setFlakes] = useState<Powerapp[]>([]);
+  const [loading, setLoading] = useState(true);
+  const { notifyError } = useMessageToast();
+  const dict = useTranslations("dict.toast");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await get("small/flakes/user", ENV.UITOOL);
+      if (response.statusCode === 200) {
+        setFlakes(response.result.powerapps);
+        setLoading(false);
+      } else {
+        notifyError(dict("error_tryagain"));
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { flakes, loading };
+};
--- src/routes/NewCollection/index.tsx
+++ src/routes/NewCollection/index.tsx
@@ -1,5 +1,46 @@
+import { useTranslations } from "next-intl";
+import styles from "./styles.module.scss";
+import Breadcrumb from "@/components/Breadcrumb";
+import Input from "@/components/Input";
+import { useState } from "react";
+
 const NewCollectionPage = () => {
-  return <section>NewCollection</section>;
+  const dict = useTranslations("dict.collections.new_collection");
+  const [form, setForm] = useState({
+    name: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+
+  return (
+    <section className={styles.new_collection}>
+      <Breadcrumb title={dict("title")} />
+      <div className={styles.container}>
+        <div className={styles.template}>
+          <div className={styles.sm_img}></div>
+          <div className={styles.lg_img}></div>
+        </div>
+        <div className={styles.form}>
+          <Input
+            type='text'
+            textLabel={dict("input")}
+            textHolder={dict("input")}
+            name='name'
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <div className={styles.select}>
+            <label>{dict("select")}</label>
+            <select>
+              <option>Hola</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default NewCollectionPage;
