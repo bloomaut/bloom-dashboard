0eff3a67dab04437bdb7d03c61eab661dfde3d3c
rumrage <claravillarruel4@gmail.com>
2024-04-04 16:02:33 -0300

    FIX show email, location & phone if exist

--- src/routes/Clients/Detail/index.tsx
+++ src/routes/Clients/Detail/index.tsx
@@ -9,18 +9,24 @@ const Detail = () => {
   return (
     <section className={styles.container}>
       <p className={styles.detail_names}>
-        {clientSelected?.ClientFirstname} {clientSelected?.ClientLastname}
+        {clientSelected?.ClientFirstname} {clientSelected?.ClientLastname && clientSelected.ClientLastname}
       </p>
       <div className={styles.info}>
-        <p className={styles.label}>
-          {dict("form_label_03")}: <span>{clientSelected?.ClientEmail}</span>
-        </p>
-        <p className={styles.label}>
-          {dict("form_label_04")}: <span>{clientSelected?.ClientLocation}</span>
-        </p>
-        <p className={styles.label}>
-          {dict("form_label_05")}: <span>{clientSelected?.ClientPhone}</span>
-        </p>
+        {clientSelected?.ClientEmail && (
+          <p className={styles.label}>
+            {dict("form_label_03")}: <span>{clientSelected?.ClientEmail}</span>
+          </p>
+        )}
+        {clientSelected?.ClientLocation && (
+          <p className={styles.label}>
+            {dict("form_label_04")}: <span>{clientSelected?.ClientLocation}</span>
+          </p>
+        )}
+        {clientSelected?.ClientPhone && (
+          <p className={styles.label}>
+            {dict("form_label_05")}: <span>{clientSelected?.ClientPhone}</span>
+          </p>
+        )}
         {clientSelected?.personalNote && (
           <p className={styles.label}>
             {dict("form_label_06")}: <span>{clientSelected?.personalNote}</span>
