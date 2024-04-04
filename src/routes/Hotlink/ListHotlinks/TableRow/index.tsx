5f83af082748f9c8586aa023fbfe9251dfa57363
rumrage <claravillarruel4@gmail.com>
2024-04-04 11:40:51 -0300

    ADD table

---
+++ src/routes/Hotlink/ListHotlinks/TableRow/index.tsx
@@ -0,0 +1,37 @@
+import styles from "./styles.module.scss";
+import { useTranslations } from "next-intl";
+import copyIcon from "/public/icons/copy.svg";
+import wpIcon from "/public/icons/whatsapp.svg";
+import Image from "next/image";
+
+interface TableRowProps {
+  data: {
+    nameHotlink: string;
+    name: string;
+    link: string;
+  };
+}
+
+const TableRow = ({ data }: TableRowProps) => {
+  const dict = useTranslations("dict.my-collection");
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.column}>
+        <p>{data.nameHotlink}</p>
+      </div>
+      <div className={styles.column}>
+        <p>{data.name}</p>
+      </div>
+      <div className={styles.column}>
+        <p>{data.link}</p>
+      </div>
+      <div className={styles.column}>
+        <Image src={copyIcon} width={30} height={30} alt='icon' />
+        <Image src={wpIcon} width={30} height={30} alt='icon' />
+      </div>
+    </div>
+  );
+};
+
+export default TableRow;
--- src/routes/Hotlink/ListHotlinks/index.tsx
+++ src/routes/Hotlink/ListHotlinks/index.tsx
@@ -1,19 +1,59 @@
 import styles from "./styles.module.scss";
 import Search from "@/components/Search";
 import { useClientsContext } from "@/context/ClientsContext";
+import TableRow from "./TableRow";
+
+const data = [
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente 1", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  { nameHotlink: "Pedidos/ Hoh-cute-cocodrile", name: "Cliente", link: "power-app-engine.vercel.app/.....2a29d4" },
+  {
+    nameHotlink: "Pedidos/ Hoh-cute-cocodrile",
+    name: "Cliente ultimo",
+    link: "power-app-engine.vercel.app/.....2a29d4",
+  },
+];
 
 const ListHotlinks = () => {
   const { searchValue, setSearchValue } = useClientsContext();
 
   return (
     <div className={styles.container}>
-      <div className={styles.text_container}>
+      <div className={styles.title_container}>
         <p>Hotlink List</p>
         <Search
           searchValue={searchValue}
           handleSearchChange={e => setSearchValue(e.target.value)}
-          placeholder='Buscar Clientes'
+          placeholder='Search'
         />
+      </div>
+      {/* TABLA */}
+      <div className={styles.table_container}>
+        <h4>Diseño/Template</h4>
+        <h4>Cliente</h4>
+        <h4>Hotlink URL</h4>
+        <h4>Compartir</h4>
+      </div>
+      <div className={styles.rows_container}>
+        {data.map(client => {
+          return <TableRow data={client} />;
+        })}
       </div>
     </div>
   );
