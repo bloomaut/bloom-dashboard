8b9c97bd52bf7416c321e91f7799a34ca63eb527
Franco Galluccio <francoivangalluccio@gmail.com>
2024-04-04 19:53:06 -0300

    FIX sequence logic

--- src/routes/Business/Sequence/index.tsx
+++ src/routes/Business/Sequence/index.tsx
@@ -19,30 +19,34 @@ const Sequence = () => {
   const files = useAppSelector(data => data.files.media);
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
   const [currentStep, setCurrentStep] = useState(1);
-  const [completedSteps, setCompletedSteps] = useState([1, 2, 3, 4]);
+  const [missingSteps, setMissingSteps] = useState([1, 2, 3, 4]);
   const dict = useTranslations("dict.business.sequence");
 
   useEffect(() => {
     if (currentStep === 1) {
       if (business?.name) {
         setCurrentStep(2);
-        setCompletedSteps(completedSteps.filter(step => step !== 1));
+        setMissingSteps(missingSteps.filter(step => step !== 1));
       }
     } else if (currentStep === 2) {
       if (business?.logo) {
         setCurrentStep(3);
-        setCompletedSteps(completedSteps.filter(step => step !== 2));
+        setMissingSteps(missingSteps.filter(step => step !== 2));
       }
     } else if (currentStep === 3) {
-      if (files?.length >= 2) {
-        const hasPDF = files.some(file => file.filetype === "application/pdf");
-        if (hasPDF) {
-          setCurrentStep(4);
-          setCompletedSteps(completedSteps.filter(step => step !== 3 && step !== 4));
-        }
+      if (files?.length >= 2 && files.some(file => file.filetype === "application/pdf")) {
+        setCurrentStep(4);
+        setMissingSteps(missingSteps.filter(step => step !== 3 && step !== 4));
+      }
+    } else if (currentStep === 4) {
+      if (files?.length <= 1) {
+        setCurrentStep(2);
+        setMissingSteps([3, 4]);
       }
     }
-  }, [business, files, currentStep, completedSteps]);
+  }, [business, files, currentStep, missingSteps]);
+
+  console.log(currentStep);
 
   const content: ContentProps[] = [
     {
@@ -77,7 +81,7 @@ const Sequence = () => {
       <Subtitle text={dict("title")} />
       <div className={styles.cards_container}>
         {content.map((data, index) => (
-          <Card key={index} data={data} disabled={completedSteps.indexOf(data.step) === -1} />
+          <Card key={index} data={data} disabled={missingSteps.indexOf(data.step) === -1} />
         ))}
       </div>
       <div className={styles.btn_container}>
