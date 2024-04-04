a0f9d55eb476213ff7c068f6dea068ec6e3b12e6
rumrage <claravillarruel4@gmail.com>
2024-04-04 16:28:05 -0300

    FIX personal note space

--- src/routes/Clients/Detail/index.tsx
+++ src/routes/Clients/Detail/index.tsx
@@ -11,24 +11,28 @@ const Detail = () => {
       <p className={styles.detail_names}>
         {clientSelected?.ClientFirstname} {clientSelected?.ClientLastname && clientSelected.ClientLastname}
       </p>
-      <div className={styles.info}>
-        {clientSelected?.ClientEmail && (
-          <p className={styles.label}>
-            {dict("form_label_03")}: <span>{clientSelected?.ClientEmail}</span>
-          </p>
-        )}
-        {clientSelected?.ClientLocation && (
-          <p className={styles.label}>
-            {dict("form_label_04")}: <span>{clientSelected?.ClientLocation}</span>
-          </p>
-        )}
-        {clientSelected?.ClientPhone && (
-          <p className={styles.label}>
-            {dict("form_label_05")}: <span>{clientSelected?.ClientPhone}</span>
-          </p>
-        )}
+      <div className={styles.info_container}>
+        <div className={styles.info}>
+          {clientSelected?.ClientEmail && (
+            <p className={styles.label}>
+              {dict("form_label_03")}: <span>{clientSelected?.ClientEmail}</span>
+            </p>
+          )}
+          {clientSelected?.ClientLocation && (
+            <p className={styles.label}>
+              {dict("form_label_04")}: <span>{clientSelected?.ClientLocation}</span>
+            </p>
+          )}
+
+          {clientSelected?.ClientPhone && (
+            <p className={styles.label}>
+              {dict("form_label_05")}: <span>{clientSelected?.ClientPhone}</span>
+            </p>
+          )}
+        </div>
+
         {clientSelected?.personalNote && (
-          <p className={styles.label}>
+          <p className={styles.personal_note}>
             {dict("form_label_06")}: <span>{clientSelected?.personalNote}</span>
           </p>
         )}
