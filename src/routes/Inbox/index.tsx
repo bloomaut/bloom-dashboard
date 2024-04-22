05a3ced7508e4154554d184b60b81d52efe42af1
Gabriel Sanchez <gszcode@gmail.com>
2024-04-22 15:51:23 -0300

    ADD style changes to the iframe

--- src/routes/Inbox/index.tsx
+++ src/routes/Inbox/index.tsx
@@ -16,24 +16,7 @@ const InboxPage = () => {
 
   return (
     <section className={styles.container}>
-      <div className={styles.title_container}>
-        <Title text={dict("title")} />
-        <div className={styles.inner_container}>
-          <h5>{dict("subtitle")}</h5>
-          <div className={styles.box}>
-            <Link href='https://play.google.com/store/apps/details?id=com.notimation.agentcaller' target='_blank'>
-              <Image src={play} alt='Google Play Logo' width={25} height={25} />
-            </Link>
-            <button onClick={() => setShowPopup(!showPopup)}>
-              <Image src={miniQr} alt='QR' width={25} height={25} />
-            </button>
-
-            {showPopup && <QrModal setShowPopup={() => setShowPopup(!showPopup)} />}
-          </div>
-        </div>
-      </div>
-
-      <iframe src='https://notimation.com/es/agents' title='Agents App' width='950' height='550'></iframe>
+      <iframe src='https://notimation.com/es/agents' title='Agents App' width='100%' height='100%'></iframe>
     </section>
   );
 };
