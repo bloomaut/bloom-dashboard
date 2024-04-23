35c8cabd53eac36f0fadc4f68cec830c6b71b8ad
Gabriel Sanchez <gszcode@gmail.com>
2024-04-23 13:19:58 -0300

    uncomment form

--- src/routes/Hotlink/index.tsx
+++ src/routes/Hotlink/index.tsx
@@ -1,7 +1,7 @@
 import styles from "./styles.module.scss";
 import { ClientsProvider } from "@/context/ClientsContext";
 import { useTranslations } from "next-intl";
-import { FlakesProvider } from "@/context/FlakesContext";
+import { FlakesProvider, useFlakesContext } from "@/context/FlakesContext";
 //Componentes
 import Title from "@/components/Title";
 import Select from "./Select";
@@ -10,6 +10,7 @@ import ListHotlinks from "./ListHotlinks";
 
 const HotlinksPage = () => {
   const dict = useTranslations("dict.hotlinks");
+  const { flakes } = useFlakesContext();
 
   return (
     <FlakesProvider>
@@ -18,7 +19,7 @@ const HotlinksPage = () => {
           <Title text={dict("title")} />
           <div className={styles.inner_container}>
             <Select />
-            {/* <Form /> */}
+            {flakes && flakes.length > 0 ? <Form /> : null}
           </div>
           <ListHotlinks />
         </section>
