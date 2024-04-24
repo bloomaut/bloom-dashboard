8b4e38ad181c7fb4f848fb0325a5334d4b804740
Kevin <kevinfiorentino.1294@gmail.com>
2024-04-24 13:53:28 -0300

    Add Inbox env

--- src/routes/Inbox/index.tsx
+++ src/routes/Inbox/index.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 const InboxPage = () => {
   return (
     <section className={styles.container}>
-      <iframe src='https://notimation.com/es/agents' title='Agents App' width='100%' height='100%'></iframe>
+      <iframe src={process.env.NEXT_PUBLIC_INBOX_URL} title='Inbox App' width='100%' height='100%'></iframe>
     </section>
   );
 };
