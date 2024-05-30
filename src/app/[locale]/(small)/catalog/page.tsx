cea2350258e9c4200786df1ab8d3aaaf96340320
Elam Cano <elamcano16@gmail.com>
2024-05-30 17:50:59 -0300

    ADD detail

--- src/app/[locale]/(small)/catalog/page.tsx
+++ src/app/[locale]/(small)/catalog/page.tsx
@@ -1,13 +1,13 @@
 "use client";
+import CatalogPage from "@/routes/Catalog";
 import LoadingSpinner from "@/components/Loading";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { usePathname } from "next/navigation";
 import PopupLogin from "@/components/PopupLogin";
-import CatalogDetail from "@/routes/CatalogDetail";
 
 export default function Page() {
   const pathname = usePathname();
   const { user, isLoading } = useUser();
 
-  return <>{!isLoading ? user ? <CatalogDetail /> : <PopupLogin currentPage={pathname} /> : <LoadingSpinner />}</>;
+  return <>{!isLoading ? user ? <CatalogPage /> : <PopupLogin currentPage={pathname} /> : <LoadingSpinner />}</>;
 }
--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,7 +1,11 @@
 import styles from "./styles.module.scss";
 
 const CatalogPage = () => {
-  return <div>Catalog</div>;
+  return (
+    <div>
+      <h1>Catalog</h1>
+    </div>
+  );
 };
 
 export default CatalogPage;
