3c6b8c27e21bb744c4f8a3dbde775cf8682f55d8
Franco Galluccio <francoivangalluccio@gmail.com>
2024-05-31 17:20:18 -0300

    FIX styles

--- src/routes/Hotlink/Form/index.tsx
+++ src/routes/Hotlink/Form/index.tsx
@@ -176,7 +176,6 @@ const Form = () => {
               <Button
                 title={dict("hotlinks.diffusion_link")}
                 styleName='btn_outline'
-                icon={<Icon name='hotlink' width={22} height={22} viewBox='0 0 30 34' className='hotlink_grey' />}
                 loading={loadingButton}
                 onclick={handleCopyClick}
               />
