eeb0cc3e939ca69614c330785dcf5c6f09b9979b
Elam Cano <elamcano16@gmail.com>
2024-06-05 16:38:56 -0300

    FIX styles

--- src/routes/CatalogDetail/TableRow/index.tsx
+++ src/routes/CatalogDetail/TableRow/index.tsx
@@ -18,16 +18,23 @@ const TableRow = ({ name, description, price, image }: Props) => {
       {image ? (
         <Image src={image} className={styles.imagen} alt={name} />
       ) : (
-        <Icon name='dataset' width={70} height={30} viewBox={"0 0 84 54"} strokeColor={"#BEBEBE"} className='dataset' />
+        <Icon
+          name='dataset'
+          width={70}
+          height={30}
+          viewBox={"0 0 84 54"}
+          strokeColor={"#BEBEBE"}
+          className='dataset_default'
+        />
       )}
       <p className={`${styles.name} ${styles.box}`}>{name}</p>
       <p className={`${styles.description} ${styles.box}`}>{description}</p>
       <div className={`${styles.price_container} ${styles.box}`}>
         <p>{`${dict("price")}-$- ${price}`}</p>
-        <div className={styles.icons}>
-          <Icon name='edit' viewBox={"0 -2 30 30"} />
-          <Icon name='delete' />
-        </div>
+      </div>
+      <div className={`${styles.icons} ${styles.box}`}>
+        <Icon name='edit' viewBox={"0 -2 30 30"} className='cursor_pointer' />
+        <Icon name='delete' className='cursor_pointer' />
       </div>
     </div>
   );
--- src/routes/CatalogDetail/index.tsx
+++ src/routes/CatalogDetail/index.tsx
@@ -8,7 +8,7 @@ import Icon from "@/components/Icon";
 import TableHead from "./TableHead";
 import { Fade } from "react-awesome-reveal";
 import TableRow from "./TableRow";
-
+import Title from "@/components/Title";
 const rows: any[] = [
   { name: "Name", description: "Description", price: 20 },
   { name: "Name", description: "Description", price: 20 },
@@ -24,44 +24,48 @@ const CatalogDetail = () => {
 
   return (
     <section className={styles.catalog_detail_container}>
-      <Breadcrumb />
-      <div className={styles.header}>
-        <div className={styles.title_container}>
-          <h1>{dict("title")}:</h1>
-          <p>Catalog X</p>
+      <div className={styles.header_container}>
+        <Breadcrumb />
+        <div className={styles.header}>
+          <div className={styles.title_container}>
+            <Title text={`${dict("title")}:`} />
+            <p>Catalog X</p>
+          </div>
+          <Button
+            title={dict("add_product")}
+            styleName='btn_orange'
+            icon={<Icon name='add' viewBox='0 0 25 20' strokeColor='#fff' />}
+          />
         </div>
-        <Button
-          title={dict("add_product")}
-          styleName='btn_orange'
-          icon={<Icon name='add' viewBox='0 0 25 20' strokeColor='#fff' />}
-        />
       </div>
-      <TableHead />
-      <div className={styles.content_container}>
-        <Fade cascade damping={0.3} triggerOnce>
-          {rows.map(dataset => {
-            return (
-              <TableRow
-                name={dataset.name}
-                description={dataset.description}
-                price={dataset.price}
-                image={dataset.image}
-              />
-            );
-          })}
-        </Fade>
-      </div>
-      <div className={styles.buttons}>
-        <Button
-          title={dict("clean_bot")}
-          styleName='btn_clean'
-          icon={<Icon name='clean' strokeColor='#7F7F7F' viewBox='0 -4 25 25' />}
-        />
-        <Button
-          title={dict("train_bot")}
-          styleName='btn_dataset'
-          icon={<Icon name='train' strokeColor='white' viewBox='0 -3 25 25' />}
-        />
+      <div className={styles.table_container}>
+        <TableHead />
+        <div className={styles.content_container}>
+          <Fade cascade damping={0.3} triggerOnce>
+            {rows.map(dataset => {
+              return (
+                <TableRow
+                  name={dataset.name}
+                  description={dataset.description}
+                  price={dataset.price}
+                  image={dataset.image}
+                />
+              );
+            })}
+          </Fade>
+        </div>
+        <div className={styles.buttons}>
+          <Button
+            title={dict("clean_bot")}
+            styleName='btn_clean'
+            icon={<Icon name='clean' strokeColor='#7F7F7F' viewBox='0 -4 25 25' />}
+          />
+          <Button
+            title={dict("train_bot")}
+            styleName='btn_dataset'
+            icon={<Icon name='train' strokeColor='white' viewBox='0 -3 25 25' />}
+          />
+        </div>
       </div>
     </section>
   );
