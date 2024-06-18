6b8d997126cf64d6c41ae60ccf39b309f542854c
diaslucia <luciadias.dev@gmail.com>
2024-06-18 11:45:53 -0300

    ADD policies

---
+++ src/routes/Policies/index.tsx
@@ -0,0 +1,34 @@
+import styles from "./styles.module.scss";
+import { useTranslations } from "next-intl";
+import { last_update, policies } from "@/utils/policies";
+import Image from "next/image";
+import logo from "@/../public/assets/logo_small_color.png";
+
+const Policies = () => {
+  const dict = useTranslations("dict.privacy");
+
+  return (
+    <div className={styles.container}>
+      <Image src={logo} alt='Small' className={styles.logo} />
+      <h2>{dict("title")}</h2>
+
+      <h3 className={styles.subtitle}>{dict("subtitle")}</h3>
+      <p className={styles.update}>
+        {dict("update")} {last_update}
+      </p>
+
+      <ol className={styles.list_container}>
+        {policies.map((policy, index) => (
+          <li className={styles.item} key={policy.id}>
+            <div className={styles.content}>
+              <h3 className={styles.item_title}>{dict(`title_0${index + 1}`)}</h3>
+              <p className={styles.item_description}>{dict(`description_0${index + 1}`)}</p>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default Policies;
