1e6c44b4bb288d1c2db0f76f688d9ec80b2175a5
Elam Cano <elamcano16@gmail.com>
2024-06-25 03:40:39 -0300

    ADD first config and left section

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,9 +1,25 @@
+import Navbar from "@/components/Navbar";
 import styles from "./styles.module.scss";
+import { useTranslations } from "next-intl";
 
 const Login = () => {
+  const dict = useTranslations("dict.login");
   return (
     <section className={styles.container}>
-      <a href='/api/auth/login'>Login</a>
+      <Navbar />
+      {/*  <a href='/api/auth/login'>Login</a> */}
+      <div className={styles.inner_container}>
+        <div className={styles.left_section}>
+          <h1 className={styles.title}>
+            <span className={styles.small}>Small</span>
+            {dict("title")}
+          </h1>
+        </div>
+        <div>
+          <div>arriba</div>
+          <div>abajo</div>
+        </div>
+      </div>
     </section>
   );
 };
