141a9f2429ba64d19a40106585e9f45ec6ce2bed
Gabriel Sanchez <gszcode@gmail.com>
2024-06-26 13:53:55 -0300

    DELETE PopupLogin component

--- src/app/[locale]/(small)/catalog/[id]/page.tsx
+++ src/app/[locale]/(small)/catalog/[id]/page.tsx
@@ -1,13 +1,10 @@
 "use client";
-import LoadingSpinner from "@/components/Loading";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import { usePathname } from "next/navigation";
-import PopupLogin from "@/components/PopupLogin";
+import LoadingSpinner from "@/components/Loading";
 import CatalogDetail from "@/routes/CatalogDetail";
 
 export default function Page() {
-  const pathname = usePathname();
   const { user, isLoading } = useUser();
 
-  return <>{!isLoading ? user ? <CatalogDetail /> : <PopupLogin currentPage={pathname} /> : <LoadingSpinner />}</>;
+  return <>{!isLoading ? user ? <CatalogDetail /> : null : <LoadingSpinner />}</>;
 }
--- src/app/[locale]/(small)/catalog/page.tsx
+++ src/app/[locale]/(small)/catalog/page.tsx
@@ -2,12 +2,9 @@
 import CatalogPage from "@/routes/Catalog";
 import LoadingSpinner from "@/components/Loading";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import { usePathname } from "next/navigation";
-import PopupLogin from "@/components/PopupLogin";
 
 export default function Page() {
-  const pathname = usePathname();
   const { user, isLoading } = useUser();
 
-  return <>{!isLoading ? user ? <CatalogPage /> : <PopupLogin currentPage={pathname} /> : <LoadingSpinner />}</>;
+  return <>{!isLoading ? user ? <CatalogPage /> : null : <LoadingSpinner />}</>;
 }
