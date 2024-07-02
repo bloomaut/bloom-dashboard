29a6ec061a1c5fe8406bbbd0df83478b4480c417
Gabriel Sanchez <gszcode@gmail.com>
2024-07-02 16:07:05 -0300

    ADD my-business page

--- src/components/DragAndDrop/index.tsx
+++ src/components/DragAndDrop/index.tsx
@@ -14,9 +14,11 @@ interface FileDragDropProps {
 
 const DragAndDrop = ({ file, setFile }: FileDragDropProps) => {
   const { notifyError } = useMessageToast();
-  const companyLogo = useAppSelector(data => data.business.logo);
+  const companyLogo = useAppSelector(data => data);
   const pathname = usePathname();
   const dict = useTranslations("dict.drag");
+
+  console.log(companyLogo);
 
   const businessPage = pathname?.includes("business");
   const myCollectionPage = pathname?.includes("my-collection");
@@ -78,8 +80,10 @@ const DragAndDrop = ({ file, setFile }: FileDragDropProps) => {
   return (
     <div {...getRootProps()} className={isDragActive ? `${styles.container} ${styles.isActive}` : styles.container}>
       <input {...getInputProps()} />
-      <Icon name='cloud' viewBox='0 0 101 91' width={60} height={60} strokeWidth={5} />
-      <p className={styles.text}>{file ? file.name : catalogPage ? `${dict("image")}` : `${dict("title")}`}</p>
+      <Icon name='cloud' viewBox='0 0 33 30' width={30} height={30} strokeWidth={3.18493} strokeColor='#1616a5' />
+      <p className={styles.text}>
+        <span>{dict("upload")}</span> {dict("drag_drop")}
+      </p>
     </div>
   );
 };
