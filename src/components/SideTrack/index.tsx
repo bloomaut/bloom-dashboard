aa3e56cc2e6c3e9e63c37f250966839a31cdb72c
diaslucia <luciadias.dev@gmail.com>
2024-07-02 15:07:40 -0300

    RESOLVE build

---
+++ src/components/SideTrack/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+
+const SideTrack = () => {
+  return (
+    <section className={styles.container}>
+      <p>SideTrack</p>
+    </section>
+  );
+};
+
+export default SideTrack;
