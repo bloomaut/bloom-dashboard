df4da141c285347ad180ba642a768899af0241c0
Gabriel Sanchez <gszcode@gmail.com>
2024-07-05 13:42:47 -0300

    ADD data to form

--- src/routes/Business/index.tsx
+++ src/routes/Business/index.tsx
@@ -2,16 +2,19 @@ import styles from "./styles.module.scss";
 import Header from "./Header";
 import MainForm from "./MainForm";
 import SecondaryForm from "./SecondaryForm";
+import { BusinessProvider } from "@/context/BusinessContext";
 
 const Business = () => {
   return (
-    <section className={styles.container_business}>
-      <Header />
-      <div className={styles.container_columns}>
-        <MainForm />
-        <SecondaryForm />
-      </div>
-    </section>
+    <BusinessProvider>
+      <section className={styles.container_business}>
+        <Header />
+        <div className={styles.container_columns}>
+          <MainForm />
+          <SecondaryForm />
+        </div>
+      </section>
+    </BusinessProvider>
   );
 };
 
