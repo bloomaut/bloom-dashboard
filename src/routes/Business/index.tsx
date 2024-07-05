27959c041f82b79e7da6b09dec526afda71b77a2
Gabriel Sanchez <gszcode@gmail.com>
2024-07-05 13:53:06 -0300

    ADD loading

--- src/routes/Business/index.tsx
+++ src/routes/Business/index.tsx
@@ -1,19 +1,10 @@
-import styles from "./styles.module.scss";
-import Header from "./Header";
-import MainForm from "./MainForm";
-import SecondaryForm from "./SecondaryForm";
+import MainBusiness from "./business";
 import { BusinessProvider } from "@/context/BusinessContext";
 
 const Business = () => {
   return (
     <BusinessProvider>
-      <section className={styles.container_business}>
-        <Header />
-        <div className={styles.container_columns}>
-          <MainForm />
-          <SecondaryForm />
-        </div>
-      </section>
+      <MainBusiness />
     </BusinessProvider>
   );
 };
