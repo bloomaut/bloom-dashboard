33378f6d1a71d597b75a12a97ac9e2b2639cd793
diaslucia <luciadias.dev@gmail.com>
2024-07-09 14:11:47 -0300

    ADD buttons to sidetrack

--- src/components/SideTrack/index.tsx
+++ src/components/SideTrack/index.tsx
@@ -1,4 +1,36 @@
+import ItemTrack from "./ItemTrack";
 import styles from "./styles.module.scss";
+
+const data = [
+  {
+    title: "Business information",
+    iconName: "business_info",
+    iconW: 22,
+    iconH: 22,
+    isActive: true,
+  },
+  {
+    title: "Select template",
+    isActive: false,
+    iconName: "select_template",
+    iconW: 20,
+    iconH: 19,
+  },
+  {
+    title: "Catalog",
+    isActive: false,
+    iconName: "select_catalog",
+    iconW: 20,
+    iconH: 20,
+  },
+  {
+    title: "Generate PowerApp",
+    isActive: false,
+    iconName: "generate_powerapp",
+    iconW: 22,
+    iconH: 12,
+  },
+];
 
 const SideTrack = () => {
   return (
@@ -8,10 +40,17 @@ const SideTrack = () => {
         <p className={styles.description}>SideTrack</p>
       </div>
       <div className={styles.steps_container}>
-        <button>Business information</button>
-        <button>Select template</button>
-        <button>Catalog</button>
-        <button>Generate PowerApp</button>
+        {data.map(btn => {
+          return (
+            <ItemTrack
+              title={btn.title}
+              iconH={btn.iconH}
+              iconW={btn.iconW}
+              iconName={btn.iconName}
+              isActive={btn.isActive}
+            />
+          );
+        })}
       </div>
     </section>
   );
