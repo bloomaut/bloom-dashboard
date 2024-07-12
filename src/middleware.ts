cfe2c3ba6a8bf607de7539aa26132e7e06c05df7
Franco Galluccio <francoivangalluccio@gmail.com>
2024-07-11 22:02:24 -0300

    ADD Protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,34 @@
 import createMiddleware from "next-intl/middleware";
 import { locales, localePrefix } from "./navigation";
+import { getSession } from "@auth0/nextjs-auth0/edge";
+import { NextRequest, NextResponse } from "next/server";
 
-export default createMiddleware({
+const intlMiddleware = createMiddleware({
   locales,
   localePrefix,
   defaultLocale: "en",
 });
+
+const protectedRoutes = ["catalog", "clients", "collections", "hotlink", "my-business", "my-collection"];
+
+function isProtectedRoute(pathname: string) {
+  return protectedRoutes.some(route => pathname.includes(route));
+}
+
+export default async function authMiddleware(req: NextRequest) {
+  const response = NextResponse.next();
+  const session = await getSession(req, response);
+
+  if (req.nextUrl.pathname === "/" && !session?.user) {
+    return NextResponse.redirect(new URL("/en", req.url));
+  }
+
+  if (!session?.user && isProtectedRoute(req.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return intlMiddleware(req);
+}
 
 export const config = {
   matcher: ["/", "/(es|en)/:path*"],
