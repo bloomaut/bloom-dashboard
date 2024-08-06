9997d0de0d39a3b4035216ff20e72e1c34c6e53f
diaslucia <luciadias.dev@gmail.com>
2024-08-06 12:43:40 -0300

    CHANGE script clarity

--- src/app/[locale]/(small)/layout.tsx
+++ src/app/[locale]/(small)/layout.tsx
@@ -7,33 +7,30 @@ import { Slide } from "react-toastify";
 import Sidebar from "@/components/Sidebar";
 import Navbar from "@/components/Navbar";
 import SideTrack from "@/components/SideTrack";
-import ClarityWrapper from "@/components/ClarityWrapper";
 
 const SmallLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClarityWrapper>
-      <div className={styles.container}>
-        <Navbar />
-        <ToastContainer
-          position='bottom-right'
-          limit={2}
-          autoClose={2000}
-          hideProgressBar
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          transition={Slide}
-        />
-        <div className={styles.inner_container}>
-          <Sidebar />
-          <SideTrack />
-          <div className={styles.children_container}>{children}</div>
-        </div>
+    <div className={styles.container}>
+      <Navbar />
+      <ToastContainer
+        position='bottom-right'
+        limit={2}
+        autoClose={2000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        transition={Slide}
+      />
+      <div className={styles.inner_container}>
+        <Sidebar />
+        <SideTrack />
+        <div className={styles.children_container}>{children}</div>
       </div>
-    </ClarityWrapper>
+    </div>
   );
 };
 
--- src/components/ClarityWrapper/index.tsx
+++ src/components/ClarityWrapper/index.tsx
@@ -1,12 +1,27 @@
 "use client";
-import { clarity } from "react-microsoft-clarity";
-import { useEffect } from "react";
+import Script from "next/script";
 
-const ClarityWrapper = ({ children }: { children: React.ReactNode }) => {
-  useEffect(() => {
-    clarity.init(`${process.env.NEXT_CLARITY_ID}`);
-  });
-  return <>{children}</>;
+const ClarityWrapper = () => {
+  return (
+    <Script
+      id='ms-clarity'
+      strategy='afterInteractive'
+      dangerouslySetInnerHTML={{
+        __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){
+                (c[a].q=c[a].q||[]).push(arguments)
+              };
+              t=l.createElement(r);
+              t.async=1;
+              t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];
+              y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", ${process.env.NEXT_CLARITY_ID});
+          `,
+      }}
+    />
+  );
 };
 
 export default ClarityWrapper;
