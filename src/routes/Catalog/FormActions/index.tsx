d82919c9e3dcf75863ee75ddcea4324a5087faa2
Gabriel Sanchez <gszcode@gmail.com>
2024-08-12 14:57:50 -0300

    ADD Form POST Catalog

--- src/routes/Catalog/FormActions/index.tsx
+++ src/routes/Catalog/FormActions/index.tsx
@@ -6,13 +6,13 @@ import CheckBox from "@/components/Checkbox";
 import DragAndDrop from "@/components/DragAndDrop";
 import Button from "@/components/Button";
 import Icon from "@/components/Icon";
+import PopupConfirm from "@/components/PopupConfirm";
 import { post, remove, update } from "@/services/fetch";
 import { ENV } from "@/typescript/types/api";
 import { useMessageToast } from "@/hooks/useMessageToast";
 import { useTranslations } from "next-intl";
 import { handleFileUpload } from "@/utils/handleFileUpload";
 import { useAppSelector } from "@/store/hooks";
-import PopupConfirm from "@/components/PopupConfirm";
 import { useCloseDropdown } from "@/hooks/useCloseDropdown";
 import { useCatalogContext } from "@/context/CatalogContext";
 
@@ -27,12 +27,14 @@ interface FormActionsProps {
 }
 
 interface InitialValuesProps {
+  type_catalog: string;
   category_name: string;
   category_description: string;
   category_image: File | null;
   category_visibility: boolean;
 }
 const initialValues: InitialValuesProps = {
+  type_catalog: "",
   category_name: "",
   category_description: "",
   category_image: null,
@@ -47,15 +49,15 @@ const FormActions = ({ setShowConfirmation, action, id, name, description, visib
   const [file, setFile] = useState<File | null>(null);
   const [closing, setClosing] = useState(false);
   const { handleRemoveDataset, handleAddDataset, handleUpdateDataset } = useCatalogContext();
-  const schema = useAppSelector(state => state.dataschema);
   const { dropdownRef } = useCloseDropdown(setShowConfirmation);
   const { notify, notifyError } = useMessageToast();
+  const schema = useAppSelector(state => state.dataschema);
   const dict = useTranslations("dict");
 
   const fieldsToValidate = ["category_name"];
   const errors = useFormValidator(formData, fieldsToValidate, file);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
     setFormData((prevState: InitialValuesProps) => ({
       ...prevState,
@@ -83,7 +85,7 @@ const FormActions = ({ setShowConfirmation, action, id, name, description, visib
         const postDataschema = {
           name: formData.category_name,
           description: formData.category_description,
-          dataschema: schema[0]._id,
+          dataschema: formData.type_catalog === "products" ? schema[0]._id : schema[1]._id,
           order: 0,
           image: null,
         };
@@ -116,6 +118,7 @@ const FormActions = ({ setShowConfirmation, action, id, name, description, visib
               throw new Error("File upload failed");
             }
           }
+
           const updatedDataset = {
             name: formData.category_name,
             order: 0,
@@ -169,6 +172,7 @@ const FormActions = ({ setShowConfirmation, action, id, name, description, visib
   useEffect(() => {
     if (action === "put") {
       setFormData({
+        type_catalog: formData.type_catalog,
         category_name: name || "",
         category_description: description || "",
         category_image: image as unknown as File | null,
@@ -195,6 +199,14 @@ const FormActions = ({ setShowConfirmation, action, id, name, description, visib
         </div>
         <div className={styles.inputs_container}>
           <div className={styles.products_information}>
+            <div className={styles.select_type}>
+              <label className={styles.label}>{dict("catalog.form_actions.catalog_type")}</label>
+              <select className={styles.select} onChange={handleChange} name='type_catalog'>
+                <option>{dict("catalog.form_actions.select_type")}</option>
+                <option>{dict("catalog.form_actions.products")}</option>
+                <option>{dict("catalog.form_actions.services")}</option>
+              </select>
+            </div>
             <Input
               type='text'
               textLabel={dict("catalog.form_actions.name")}
