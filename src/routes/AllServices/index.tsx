a95aec71274871f1f93d13f0214f57499e4c0a60
diaslucia <luciadias.dev@gmail.com>
2024-08-22 12:22:13 -0300

    REMOVE unused functions

--- src/routes/AllServices/index.tsx
+++ src/routes/AllServices/index.tsx
@@ -24,14 +24,6 @@ const AllServices = () => {
     }
   };
 
-  const handleDeleteItem = (deletedId: string) => {
-    setServices((prevItems: any) => prevItems.filter((item: any) => item._id !== deletedId));
-  };
-
-  const handleUpdateItem = (updatedItem: DataItemsServiceType) => {
-    setServices(prevItems => prevItems.map(item => (item._id === updatedItem._id ? updatedItem : item)));
-  };
-
   useEffect(() => {
     fetchServices();
   }, []);
