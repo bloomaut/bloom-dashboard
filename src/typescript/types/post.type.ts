8dffab7e424ec62b2558b52cb96f26ce7cf677be
Franco Galluccio <francoivangalluccio@gmail.com>
2024-08-26 14:20:47 -0300

    DELETE collections page

--- src/typescript/types/post.type.ts
+++ src/typescript/types/post.type.ts
@@ -1,6 +1,5 @@
 import { PostDataSchema, PostDataItem, PostOnboarding } from "../interfaces/catalog.interface";
 import { PostClient } from "../interfaces/clients.interface";
-import { PostCollection } from "../interfaces/collection.interface";
 import { PostFlake } from "../interfaces/flakes.interface";
 
-export type POST = PostClient | PostFlake | PostCollection | PostDataSchema | PostDataItem | PostOnboarding | string;
+export type POST = PostClient | PostFlake | PostDataSchema | PostDataItem | PostOnboarding | string;
