34cfe2878596f13cc3d13b9d41f4256aac63aca8
diaslucia <luciadias.dev@gmail.com>
2024-08-30 17:24:06 -0300

    ADD animation to login

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -6,17 +6,25 @@ import small from "@/../public/assets/logo_small_login.png";
 import notimation from "@/../public/assets/notimation_logo_black.png";
 import LinkComponent from "@/components/LinkComponent";
 import { useTranslations } from "next-intl";
+import { useLocale } from "next-intl";
 
 const Login = () => {
   const dict = useTranslations("dict.login");
+  const locale = useLocale();
 
   return (
     <section>
       <Navbar />
       <div className={styles.login_container}>
-        <div className={styles.description}>
-          <div className={styles.text}>
-            <span className={styles.small}>Small</span> {dict("description")}
+        <div className={styles.column_one}>
+          <div className={styles.bar}></div>
+          <div className={styles.wrapper}>
+            <div className={styles.text_container}>
+              <p className={locale === "es" ? styles.text_animation_es : styles.text_animation_en}>
+                <span className={styles.small}>Small</span> {dict("description_one")}{" "}
+              </p>
+              <p className={styles.text}>{dict("description_two")}</p>
+            </div>
           </div>
         </div>
         <div className={styles.login_info}>
