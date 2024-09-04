f14bf1ed637d087a8f44a06d8cfe1029bb4b6fa0
Franco Galluccio <francoivangalluccio@gmail.com>
2024-09-04 14:56:10 -0300

    ADD Translations

--- src/routes/Designs/Header/index.tsx
+++ src/routes/Designs/Header/index.tsx
@@ -4,6 +4,8 @@ import Search from "@/components/Search";
 import Image, { StaticImageData } from "next/image";
 import Icon from "@/components/Icon";
 import uitrade_logo from "/public/assets/logo_uitrade.svg";
+import { useTranslations } from "next-intl";
+import { useState } from "react";
 
 interface MenusProps {
   icon?: JSX.Element;
@@ -11,44 +13,57 @@ interface MenusProps {
   image?: StaticImageData;
 }
 
-const menus: MenusProps[] = [
-  {
-    icon: <Icon name='design_2' width={25} height={25} viewBox='0 0 23 25' strokeWidth={1.5} />,
-    label: "My designs",
-  },
-  {
-    icon: <Icon name='diffusion' width={25} height={25} viewBox='0 0 30 35' strokeWidth={1.5} />,
-    label: "Diffusion",
-  },
-  {
-    icon: <Icon name='mail' width={28} height={28} viewBox='0 0 37 30' strokeWidth={2.2} />,
-    label: "Email",
-  },
-  {
-    icon: <Icon name='post' width={25} height={25} viewBox='0 0 35 27' strokeWidth={0.5} fillColor='#381D2A' />,
-    label: "Post",
-  },
-  {
-    image: uitrade_logo,
-    label: "More",
-  },
-];
-
 const Header = () => {
+  const [selectedMenu, setSelectedMenu] = useState<number | null>(null);
+  const dict = useTranslations("dict.designs.header");
+
+  const menus: MenusProps[] = [
+    {
+      icon: <Icon name='design_2' width={25} height={25} viewBox='0 0 23 25' strokeWidth={1.5} />,
+      label: dict("my_designs"),
+    },
+    {
+      icon: <Icon name='diffusion' width={25} height={25} viewBox='0 0 30 35' strokeWidth={1.5} />,
+      label: dict("diffusion"),
+    },
+    {
+      icon: <Icon name='mail' width={28} height={28} viewBox='0 0 37 30' strokeWidth={2.2} />,
+      label: "Email",
+    },
+    {
+      icon: <Icon name='post' width={25} height={25} viewBox='0 0 35 27' strokeWidth={0.5} fillColor='#381D2A' />,
+      label: "Post",
+    },
+    {
+      image: uitrade_logo,
+      label: dict("more"),
+    },
+  ];
+
+  const handleMenuClick = (index: number) => {
+    if (index !== 4) {
+      setSelectedMenu(index);
+    }
+  };
+
   return (
     <div className={styles.header}>
-      <Title text='Designs' />
+      <Title text={dict("title")} />
       <div className={styles.inner_container}>
         <Search
           handleSearchChange={() => {
-            console.log("asd");
+            console.log("design");
           }}
-          placeholder='Search in your designs or in small...'
+          placeholder={dict("search_placeholder")}
           searchValue=''
         />
         <div className={styles.menus}>
           {menus.map((menu, index) => (
-            <div className={styles.menu_item} key={index}>
+            <div
+              className={`${styles.menu_item} ${selectedMenu === index ? styles.selected : ""}`}
+              key={index}
+              onClick={() => handleMenuClick(index)}
+            >
               <div className={styles.menu}>
                 {menu.icon && <div className={styles.icon_container}>{menu.icon}</div>}
                 {menu.image && <Image src={menu.image.src} width={30} height={30} alt={menu.label} />}
