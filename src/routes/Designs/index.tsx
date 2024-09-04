d97dfe84d8b5cf4fbe5f3d24669a91b5a4548bc3
diaslucia <luciadias.dev@gmail.com>
2024-09-04 10:12:49 -0300

    ADD new page

---
+++ src/routes/Designs/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.scss";
+
+const Designs = () => {
+  return (
+    <section className={styles.container}>
+      <p>Banner azul</p>
+      <p>Lista de filtros y caruseles</p>
+    </section>
+  );
+};
+
+export default Designs;
