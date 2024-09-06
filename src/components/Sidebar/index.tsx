ea7497c917711581049936def5393470071ead49
Elam Cano <elamcano16@gmail.com>
2024-09-06 09:11:06 -0300

    ADD svg and link in sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -59,6 +59,11 @@ const Sidebar = () => {
       icon: <Icon name='clients' />,
       path: "/clients",
     },
+    {
+      title: `${dict("designs")}`,
+      icon: <Icon name='designs' width={30} viewBox='0 -5 35 35' />,
+      path: "/designs",
+    },
   ];
 
   const getUserData = async () => {
