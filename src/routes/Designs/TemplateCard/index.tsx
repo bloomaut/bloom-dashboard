3ada27b3c144db733a9f97aa1e975cbdf91b505c
diaslucia <luciadias.dev@gmail.com>
2024-09-09 10:57:16 -0300

    FIX color menu card

--- src/routes/Designs/TemplateCard/index.tsx
+++ src/routes/Designs/TemplateCard/index.tsx
@@ -22,7 +22,7 @@ const TemplateCard = ({ _id, title, thumbnail }: HogRelated) => {
           {openPopup && (
             <p className={styles.create}>
               <button onClick={() => alert("holi")}>
-                <Icon name='design_2' viewBox='0 0 25 20' strokeColor='#7f7f7f' />
+                <Icon name='design_2' viewBox='0 0 25 20' strokeColor='#381d2a' />
                 {dict("create_design")}
               </button>
             </p>
