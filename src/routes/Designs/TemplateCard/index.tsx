75d49e9a5bb5e51bc0197d22e4e157e625e11915
Gabriel Sanchez <gszcode@gmail.com>
2024-09-09 09:52:34 -0300

    UPDATE icon

--- src/routes/Designs/TemplateCard/index.tsx
+++ src/routes/Designs/TemplateCard/index.tsx
@@ -22,7 +22,7 @@ const TemplateCard = ({ _id, title, thumbnail }: HogRelated) => {
           {openPopup && (
             <p className={styles.create}>
               <button onClick={() => alert("holi")}>
-                <Icon name='edit' viewBox='0 0 25 20' strokeColor='#7f7f7f' />
+                <Icon name='design_2' viewBox='0 0 25 20' strokeColor='#7f7f7f' />
                 {dict("create_design")}
               </button>
             </p>
