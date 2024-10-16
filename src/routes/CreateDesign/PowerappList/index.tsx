f56af1b6e6dd792f74df2e4a35c971c209fc4a8d
Kevin <kevinfiorentino.1294@gmail.com>
2024-10-16 16:24:43 -0300

    Fix

--- src/routes/CreateDesign/PowerappList/index.tsx
+++ src/routes/CreateDesign/PowerappList/index.tsx
@@ -69,7 +69,7 @@ const PowerappList = ({ setActiveStep }: PowerappListProps) => {
     const dataToSend = {
       pwa_id: powerAppSelected,
       flake_id: flakeId || String(params.id),
-      type_design: listTemplates?.type.toLowerCase() || "",
+      type_design: selectedList,
     };
 
     const response = await post("design-small/prepare", dataToSend);
