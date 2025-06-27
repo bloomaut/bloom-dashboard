48fb2a26bcadd39b9a6462909e5d2d53e2175a7f
Razgrizsx <razgrisss41@gmail.com>
2025-06-27 13:22:37 -0300

    finances

--- src/components/v0Components/SectionBlocked.tsx
+++ src/components/v0Components/SectionBlocked.tsx
@@ -9,7 +9,7 @@ interface SectionBlockedProps {
 
 export function SectionBlocked({ title, description }: SectionBlockedProps) {
   return (
-    <div className='flex-1 flex flex-col overflow-hidden'>
+    <div className='flex-1 flex flex-col overflow-hidden '>
       {/* Header */}
       <header className='bg-white border-b border-gray-200 px-6 py-4'>
         <div className='flex items-center justify-between'>
@@ -19,7 +19,7 @@ export function SectionBlocked({ title, description }: SectionBlockedProps) {
 
       {/* Main Content */}
       <main className='flex-1 overflow-auto p-6 flex items-center justify-center'>
-        <div className='mx-auto w-full'>
+        <div className='mx-auto '>
           <TutorialStepper />
         </div>
       </main>
