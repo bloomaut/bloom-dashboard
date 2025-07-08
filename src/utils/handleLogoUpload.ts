70c1616d7bcf49d4d358d175c891f120a320df66
Razgrizsx <razgrisss41@gmail.com>
2025-07-08 18:53:38 -0300

    inventario a medio hacer

--- src/utils/handleLogoUpload.ts
+++ src/utils/handleLogoUpload.ts
@@ -1,11 +1,7 @@
 import { putFile } from "@/services/fetch";
 import { ENV } from "@/typescript/types/api";
 
-export async function handleLogoUpload(
-  file: File,
-  notify: (msg: string) => void,
-  notifyError: (msg: string) => void
-) {
+export async function handleLogoUpload(file: File, notify: (msg: string) => void, notifyError: (msg: string) => void) {
   if (file) {
     const response = await putFile("small-business/logo", file, ENV.DASHBOARD);
     if (response.data.statusCode === 200) {
