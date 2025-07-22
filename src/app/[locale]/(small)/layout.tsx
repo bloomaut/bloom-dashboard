31c4a237ebfa09f07de2336815ddb2512c345c2f
Razgrizsx <razgrisss41@gmail.com>
2025-07-22 11:24:17 -0300

    questinare enabled

--- src/app/[locale]/(small)/layout.tsx
+++ src/app/[locale]/(small)/layout.tsx
@@ -34,23 +34,21 @@ const SmallLayout = ({ children }: { children: React.ReactNode }) => {
           pauseOnHover
           transition={Slide}
         />
-        {
-          /* user.client.proposal_status !== "APPROVED" */ false ? (
-            <div className={styles.inner_container}>
-              <div className={styles.children_container} id='children_container'>
-                <Questionaire />
-              </div>
+        {user.client.proposal_status !== "APPROVED" ? (
+          <div className={styles.inner_container}>
+            <div className={styles.children_container} id='children_container'>
+              <Questionaire />
             </div>
-          ) : (
-            <div className={styles.inner_container}>
-              <Sidebar />
-              <SideTrack />
-              <div className={styles.children_container} id='children_container'>
-                {children}
-              </div>
+          </div>
+        ) : (
+          <div className={styles.inner_container}>
+            <Sidebar />
+            <SideTrack />
+            <div className={styles.children_container} id='children_container'>
+              {children}
             </div>
-          )
-        }
+          </div>
+        )}
       </TutorialProvider>
     </div>
   );
