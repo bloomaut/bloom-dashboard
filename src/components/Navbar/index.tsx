686a9c0d040893d15dbe061a9bf14feee41cd116
Razgrizsx <razgrisss41@gmail.com>
2025-07-24 10:33:49 -0300

    test

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -21,6 +21,7 @@ const Navbar = () => {
 
   return (
     <nav className={styles.container}>
+      Hola
       <div className={styles.logo_container}>
         <Link href='/'>
           <Image src={small} alt='Small' className={styles.logo} width={300} height={300} priority />
