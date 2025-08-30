2b85f0039f481ef8a6b8f5a9cbec65519660deb7
Homero Gazze <homerogazze1015@gmail.com>
2025-08-30 12:59:12 -0300

    VSL video turn off temporaly

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -94,7 +94,7 @@ export default function SmallLanding() {
             />
           ) : (
             <iframe
-              src='https://player.vimeo.com/video/1110327162?autoplay=1&title=0&byline=0&portrait=0'
+              src='https://player.vimeo.com/video/111032716?autoplay=1&title=0&byline=0&portrait=0' // 'https://player.vimeo.com/video/1110327162?autoplay=1&title=0&byline=0&portrait=0'
               width='1056'
               height='594'
               frameBorder='0'
