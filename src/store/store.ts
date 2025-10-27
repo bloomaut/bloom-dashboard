c49a92abd87402f1fd40daa3ac70cfe07ef0a6e9
Homero Gazze <homerogazze1015@gmail.com>
2025-10-27 13:10:09 -0300

    chore: remove unused assets, interfaces, and utils
    refactor: clean up store and remove unused slices
    fix: update OnboardingGuard logic and remove temporary comment

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./features/userSlice";
 import dataSchemaSlice from "./features/dataschemaSlice";
-import ricardoSlice from "./features/ricardoSlice";
-import subdomainsSlice from "./features/subdomainsSlice";
 import questSlice from "../features/(onboarding)/Questionary/store/questSlice";
 import socialMediaSlice from "@/features/(dashboard)/Social/store/socialMediaSlice";
 
@@ -10,8 +8,6 @@ export const store = configureStore({
   reducer: {
     userData: userSlice,
     dataschema: dataSchemaSlice,
-    ricardosData: ricardoSlice,
-    subdomainsData: subdomainsSlice,
     questData: questSlice,
     socialMedia: socialMediaSlice,
   },
--- src/typescript/interfaces/user.interface.ts
+++ src/typescript/interfaces/user.interface.ts
@@ -26,14 +26,3 @@ export interface UserDataProps {
   password: string;
   phone: string;
 }
-
-export interface Subdomain {
-  id: number;
-  base_url: string;
-  subdomain: string;
-  full_domain: string;
-  registration: "free" | "purchased";
-  created_at: Date;
-  updated_at: Date;
-  flake_landing: null | { _id: string };
-}
